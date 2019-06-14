4cb56ee77860de28ba18ee616ae432f3130d9f83
DFolchA <daniel.folch@wazuh.com>
2019-06-14 15:43:20 +0200

    Added checksum generation, and built msi using wixtool

--- windows/entrypoint.sh
+++ windows/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Wazuh package builder
 # Copyright (C) 2015-2019, Wazuh Inc.
 #
@@ -6,17 +6,20 @@
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
-
+set -exf
 wazuh_version=$1
-revision=$2
-msi_name="wazuh-agent-${wazuh_version}-${revision}"
-
+rm -rf /home/wazuh_msi/output/*
+ls /home/wazuh_msi/output
 git clone https://github.com/wazuh/wazuh.git
+
 cd wazuh/src
 git checkout ${wazuh_version}
 
 make deps
 make TARGET=winagent
 
+wget https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip
+unzip wix311-binaries.zip -d /home/wazuh_msi/wazuh/src/win32/wix
+
 cd  "/home/wazuh_msi/"
-mv ${msi_name} ""
+cp -rf "/home/wazuh_msi/wazuh" "/home/wazuh_msi/output"
--- windows/generate_wazuh_msi.ps1
+++ windows/generate_wazuh_msi.ps1
@@ -3,9 +3,10 @@ param (
     [string]$BRANCH_TAG = "",
     [string]$REVISION = "",
     [string]$DESTINATION = "",
-    [switch]$CHECKSUM,
     #Optionals
     [Int]$JOBS = 4,
+    [string]$CHECKSUM="",
+    [switch]$SIGN,
     [switch]$help
     )
 
@@ -17,10 +18,52 @@ param (
         -REVISION <REV>             [Required] Package revision that append to version e.g. x.x.x-rev
         -JOBS <N_JOBS>              [Optional] Number of parallel jobs when compiling.
         CHECKSUM                    [Optional] Generate checksum file for the generated package.
-        -h, --help                  Show this help.
+        -help                       Show this help.
         "
         Exit
     }
 
-    docker build -t wazuhmsi .
-    docker run -t --rm wazuhmsi $BRANCH_TAG $REVISION
+    if($BRANCH_TAG -eq "" -or $REVISION -eq "" -or $DESTINATION -eq ""){
+        if($BRANCH_TAG -eq "")  {"        BRANCH_TAG is needed"}
+        if($REVISION -eq "")    {"        REVISION is needed"}
+        if($DESTINATION -eq "") {"        DESTINATION is needed"}
+        "
+        Usage: generate_wazuh_msi.ps1 -BRANCH_TAG <BRANCH> -REVISION <REV> -JOBS <N_JOBS>
+        Arguments description:
+        -BRANCH_TAG <BRANCH>        [Required] Select Git branch or tag e.g. $BRANCH
+        -REVISION <REV>             [Required] Package revision that append to version e.g. x.x.x-rev
+        -JOBS <N_JOBS>              [Optional] Number of parallel jobs when compiling.
+        -SIGN                       [Optional] Sign package
+        -CHECKSUM                   [Optional] Generate checksum file for the generated package.
+        -help                       Show this help.
+        "
+        Exit
+    }
+
+    else{
+        [string]$MSI_NAME="wazuh-agent-${VERSION}-${REVISION}.msi"
+        [string]$DESTINATION2=[string]$DESTINATION -replace "C:\\","/c/"
+
+        [string]$DESTINATION=[string]$DESTINATION -replace "\", "/"
+
+        docker build -t wazuhmsi .
+        docker run -it -v ${DESTINATION2}:/home/wazuh_msi/output wazuhmsi $BRANCH_TAG $REVISION
+
+        cd $DESTINATION\wazuh\wazuh\src\win32
+         if(($SIGN.isPresent)) {
+            signtool sign /a /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 "*.exe"
+            signtool sign /a /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 "InstallerScripts.vbs"
+         }
+        .\wix\candle.exe -nologo "wazuh-installer.wxs" -out "wazuh-installer.wixobj" -ext WixUtilExtension -ext WixUiExtension
+
+        .\wix\light.exe "wazuh-installer.wixobj" -out "$DESTINATION\$MSI_NAME"  -ext WixUtilExtension -ext WixUiExtension
+         cd $DESTINATION
+        if(($SIGN.isPresent)) {
+            signtool sign /a /tr http://rfc3161timestamp.globalsign.com/advanced /d "$MSI_NAME" /td SHA256 "$MSI_NAME"
+        }
+        if(!($CHECKSUM -eq "")) {
+            Set-Content -Path "$CHECKSUM\$MSI_NAME.sha512" -Value (Get-FileHash -Path "$DESTINATION\$MSI_NAME" -Algorithm SHA512)
+        }
+        Remove-Item .\wazuh\ -Recurse
+        
+    }
