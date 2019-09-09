bfe18c8fe9ecdb4ebdfdfdaf9c68012cbd9d0a61
dfolcha <daniel.folch@wazuh.com>
2019-09-09 17:46:59 +0200

    Fix wrong variable call in checkinstall.sh

--- solaris/solaris10/checkinstall.sh
+++ solaris/solaris10/checkinstall.sh
@@ -4,7 +4,7 @@
 expected_platform="i386"
 platform=`uname -p`
 if [ ${platform} != ${expected_platform} ]; then
-        echo "This package must be installed on ${expected_platform}"
+        echo "This package must be installed on ${platform}"
         exit
 fi
 exit 0
