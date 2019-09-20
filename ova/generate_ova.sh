f531ec763fd75f8e4e7a2c0bdc23f766565e4c45
dfolcha <daniel.folch@wazuh.com>
2019-09-20 16:46:53 +0200

    Delete VM after exporting and clean if build_ova fails

--- ova/generate_ova.sh
+++ ova/generate_ova.sh
@@ -83,7 +83,7 @@ build_ova() {
     VM_EXPORT=$(vboxmanage list vms | grep -i vm_wazuh | cut -d "\"" -f2)
     # OVA creation with all metadata information.
     vboxmanage export ${VM_EXPORT} -o ${OVA_VM} --vsys 0 --product "Wazuh v${WAZUH_VERSION} OVA" --producturl "https://packages.wazuh.com/vm/wazuh${OVA_VERSION}.ova" --vendor "Wazuh, inc <info@wazuh.com>" --vendorurl "https://wazuh.com" --version "$OVA_VERSION" --description "Wazuh helps you to gain security visibility into your infrastructure by monitoring hosts at an operating system and application level. It provides the following capabilities: log analysis, file integrity monitoring, intrusions detection and policy and compliance monitoring." || clean 1
-
+    vagrant destroy -f
     tar -xvf ${OVA_VM}
 
     python Ova2Ovf.py -s ${OVA_VM} -d ${OVA_FIXED}
@@ -205,7 +205,7 @@ main() {
         OVA_VERSION="${WAZUH_VERSION}_${ELK_VERSION}"
 
         echo "Version to build: ${WAZUH_VERSION}-${ELK_VERSION} with ${STATUS} repository."
-        build_ova ${WAZUH_VERSION} ${OVA_VERSION}
+        build_ova ${WAZUH_VERSION} ${OVA_VERSION} || clean 1
     else
         echo "ERROR: Need more parameters."
         help 1
