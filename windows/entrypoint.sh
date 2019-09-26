459f514c039fb6df44ba8ec90f6b287e696f5ade
jmv74211 <jonathan.martin@wazuh.com>
2019-09-26 11:21:14 +0200

    Added agent compilation in windows.
    Added script to automate and generate a zip with the wazuh agent compiled according to some parameters.

---
+++ windows/entrypoint.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+set -ex
+
+BRANCH=$1
+JOBS=$2
+DEBUG=$3
+
+URL_REPO=https://github.com/wazuh/wazuh/archive/${BRANCH}.zip
+
+# Download the wazuh repository
+wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
+
+# Compile the wazuh agent for Windows
+FLAGS="-j ${JOBS} "
+
+if [[ "${DEBUG}" = "yes" ]]; then
+    FLAGS+="-g "
+
+make -C /wazuh-*/src deps ${FLAGS} 
+make -C /wazuh-*/src TARGET=winagent ${FLAGS} 
+
+# Zip the compiled agent and move it to the shared folder
+zip -r wazuh-${BRANCH}.zip wazuh-*
+cp  wazuh-${BRANCH}.zip /shared
