5b82411524b0128bc16370fd101f9a9f818abd81
dfolcha <daniel.folch@wazuh.com>
2020-07-31 13:07:47 +0200

    Use opendistro to provision the OVA

--- ova/provision.sh
+++ ova/provision.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-set -exf
+set -x
 # Variables
 repo_branch=$(echo "$1" | cut -c1-3)
 repo_baseurl=$(echo "$1" | cut -c1-2)
@@ -11,8 +11,7 @@ ELK_MAJOR=`echo ${ELK_VERSION}|cut -d"." -f1`
 ELK_MINOR=`echo ${ELK_VERSION}|cut -d"." -f2`
 config_files="/vagrant/Config_files"
 
-. /vagrant/Libraries/wazuh_functions.sh
-. /vagrant/Libraries/elastic_functions.sh
+. /vagrant/Libraries/provision-opendistro.sh
 
 # Setting wazuh default root password
 yes wazuh | passwd root
@@ -25,13 +24,19 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 # Dependences
 yum install openssl -y
 
-install_wazuh
-
-elastic_stack_${ELK_MAJOR}
+healthCheck   
+installPrerequisites
+addWazuhrepo
+installWazuh
+installElasticsearch
+installFilebeat
+installKibana
+checkInstallation  
 
 rm -rf /vagrant
 
 systemctl stop kibana
+systemctl filebeat kibana
 systemctl stop  elasticsearch
 systemctl stop wazuh-manager
 systemctl stop wazuh-api
