47ce87ab9878f228e83773053d1d806814ed9369
Víctor Rebollo <victorrebollop@gmail.com>
2020-09-07 09:07:43 +0200

    Replaced0 < used for string comparasion with -lt

--- ova/Libraries/provision-opendistro.sh
+++ ova/Libraries/provision-opendistro.sh
@@ -247,7 +247,7 @@ healthCheck() {
     cores=$(nproc)
     ram_gb=$(free -m | awk '/^Mem:/{print $2}')
 
-    if [[ $cores < "2" ]] || [[ $ram_gb < "4096" ]]; then
+    if [[ $cores -lt 2 ]] || [[ $ram_gb -lt 4096 ]]; then
         echo "The system must have at least 4Gb of RAM and 2 CPUs"
         exit 1;
     else
