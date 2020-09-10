da45e8a3f81bc44d0e70e840e1e2c054fbcc3665
Víctor Rebollo <victorrebollop@gmail.com>
2020-09-10 13:49:05 +0200

    Script automatic_set_ram, fixed error catching gigabytes

--- ova/Libraries/automatic_set_ram.sh
+++ ova/Libraries/automatic_set_ram.sh
@@ -2,5 +2,5 @@ etc_elastic="/etc/elasticsearch"
 ram_gb=$(free -m | awk '/^Mem:/{print $2}')
 ram=$(( ${ram_gb} / 2 ))
 
-sed -i "s/-Xms[0-9]\+m/-Xms${ram}m/" ${etc_elastic}/jvm.options
-sed -i "s/-Xmx[0-9]\+m/-Xmx${ram}m/" ${etc_elastic}/jvm.options
+sed -i "s/-Xms[0-9]\+[gm]/-Xms${ram}m/" "${etc_elastic}/jvm.options"
+sed -i "s/-Xmx[0-9]\+[gm]/-Xmx${ram}m/" "${etc_elastic}/jvm.options"
