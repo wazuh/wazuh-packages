8e4abff881eb9f83663195c31a86b50e5bbe1288
José Fernández <pylott@gmail.com>
2021-02-12 10:34:21 +0100

    Fix 5.0.0 SPECs (CMake requirement)

--- macos/package_files/5.0.0/postinstall.sh
+++ macos/package_files/5.0.0/postinstall.sh
@@ -109,7 +109,7 @@ fi
 ${INSTALLATION_SCRIPTS_DIR}/src/init/register_configure_agent.sh ${DIR} > /dev/null || :
 
 # Install the service
-${INSTALLATION_SCRIPTS_DIR}/src/init/darwin-init.sh
+${INSTALLATION_SCRIPTS_DIR}/src/init/darwin-init.sh ${DIR}
 
 # Remove temporary directory
 rm -rf ${DIR}/packages_files
