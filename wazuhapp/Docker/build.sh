95f090496754cab08dfb2f32647b1dfd0dd23444
Santiago Bruno <santiago.bruno@wazuh.com>
2021-03-07 16:47:26 -0300

    Modifying package.json to use --allow-root in kibana bootstrap and not doing the git hook Installing zip in Dockerfile

--- wazuhapp/Docker/build.sh
+++ wazuhapp/Docker/build.sh
@@ -86,7 +86,8 @@ download_kibana_sources() {
 install_dependencies () {
     cd ${kibana_dir}
     change_node_version $kibana_node_version
-    npm install -g "yarn@${kibana_yarn_version}"    
+    npm install -g "yarn@${kibana_yarn_version}"
+    sed -i 's/node scripts\/build_ts_refs && node scripts\/register_git_hook/node scripts\/build_ts_refs --allow-root/' ${kibana_dir}/package.json
     yarn kbn bootstrap --skip-kibana-plugins --oss --allow-root
 }
 
@@ -129,3 +130,4 @@ download_kibana_sources
 install_dependencies
 download_wazuh_app_sources
 build_package
+
