82237f09ea4a66516f1d7f28be6b74c286ea2763
José Fernández <pylott@gmail.com>
2021-04-20 10:29:19 +0200

    Removed wazuh_modules folder removal

--- wpk/run.sh
+++ wpk/run.sh
@@ -208,7 +208,7 @@ clean() {
     rm -rf src/{addagent,analysisd,client-agent,config,error_messages,external/*}
     rm -rf src/{headers,logcollector,monitord,os_auth,os_crypto,os_csyslogd}
     rm -rf src/{os_dbdos_execd,os_integrator,os_maild,os_netos_regex,os_xml,os_zlib}
-    rm -rf src/{remoted,reportd,shared,syscheckd,tests,update,wazuh_db,wazuh_modules}
+    rm -rf src/{remoted,reportd,shared,syscheckd,tests,update,wazuh_db}
 
     if [[ "${BUILD_TARGET}" != "winagent" ]]; then
         rm -rf src/win32
