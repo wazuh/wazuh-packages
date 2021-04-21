c9b6e6ae3276ccb0efb2b7468e9b822aa13bf6f0
rauldpm <raul.delpozo@wazuh.com>
2021-04-21 21:22:16 +0200

    Added usage example

--- ova/generate_ova.sh
+++ ova/generate_ova.sh
@@ -8,6 +8,10 @@
 # and/or modify it under the terms of the GNU General Public
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
+
+# ./generate_ova.sh -v 4.1.4 -o 1.12.0 -f 7.10.0 -r dev -b wj-2421-ova-rework -p 4.1
+
+
 set -e
 # Dependencies: vagrant, virtualbox, ovftool
 
@@ -92,6 +96,9 @@ build_ova() {
     vagrant up || clean 1
     vagrant suspend
     
+    # Pause creation and check installation
+    # TODO
+
     # OVA creation with all metadata information.
     VM_EXPORT=$(vboxmanage list vms | grep -i vm_wazuh | cut -d "\"" -f2)
     vboxmanage export ${VM_EXPORT} -o ${OVA_VM} --vsys 0 --product "Wazuh v${WAZUH_VERSION} OVA" --producturl "https://packages.wazuh.com/vm/wazuh-${OVA_VERSION}.ova" --vendor "Wazuh, inc <info@wazuh.com>" --vendorurl "https://wazuh.com" --version "$OVA_VERSION" --description "Wazuh helps you to gain security visibility into your infrastructure by monitoring hosts at an operating system and application level. It provides the following capabilities: log analysis, file integrity monitoring, intrusions detection and policy and compliance monitoring." || clean 1
