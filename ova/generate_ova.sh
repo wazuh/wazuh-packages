04c6e2fe8aca79fcbb18222c4cc170ae84892dd1
rauldpm <raul.delpozo@wazuh.com>
2021-04-29 15:44:57 +0200

    Eliminated unnecessary parameters

--- ova/generate_ova.sh
+++ ova/generate_ova.sh
@@ -24,14 +24,12 @@ CHECKSUM_DIR=""
 HAVE_VERSION=false
 HAVE_OPENDISTRO_VERSION=false
 HAVE_ELK_VERSION=false
-HAVE_PACKAGE_VERSION=false
 
-PACKAGE_VERSION=""
 WAZUH_VERSION=""
 OPENDISTRO_VERSION=""
-BRANCH="master"
 ELK_VERSION=""
 PACKAGES_REPOSITORY=""
+BRANCH=""
 CHECKSUM="no"
 UI_REVISION="1"
 
@@ -39,18 +37,16 @@ help () {
 
     echo
     echo "Usage: $0 [OPTIONS]"
-    echo "  -p, --package          [Required] Select Git branch or tag from wazuh-packages repository."
     echo "  -w, --wazuh            [Required] Version of wazuh to install on VM."
     echo "  -o, --opendistro       [Required] Version of Open Distro for Elasticsearch."
     echo "  -f, --filebeat         [Required] Filebeat's version."
     echo "  -r, --repository       [Required] Select the software repository [prod/dev]."
-    echo "  -b, --branch           [Optional] Branch/tag of the Wazuh repository. By Default: master"
     echo "  -s, --store <path>     [Optional] Set the destination absolute path of package."
     echo "  -c, --checksum <path>  [Optional] Generate checksum. By Default: no"
     echo "  -u, --ui-revision      [Optional] Revision of the UI package. By default, 1."
     echo "  -h, --help             [  Util  ] Show this help."
     echo
-    echo "Use example: ./generate_ova.sh -p 4.1 -w 4.1.5 -o 1.12.0 -f 7.10.0 -r prod -b wj-2421-ova-rework"
+    echo "Use example: ./generate_ova.sh -w 4.1.5 -o 1.12.0 -f 7.10.0 -r prod"
     exit $1
 }
 
@@ -71,6 +67,7 @@ build_ova() {
     OVA_FIXED="wazuh-${OVA_VERSION}-fixed.ova"
     OVA_VMDK="wazuh-${OVA_VERSION}-disk001.vmdk"
     OVA_VDI="wazuh-${OVA_VERSION}-disk001.vdi"
+
 
     # Delete OVA/OVF files if exists
     if [ -e "${OUTPUT_DIR}/${OVA_VM}" ] || [ -e "${OUTPUT_DIR}/${OVF_VM}" ]; then
@@ -145,8 +142,8 @@ check_version() {
         elif [ "${PACKAGES_REPOSITORY}" = "dev" ]; then
             curl -Isf https://packages-dev.wazuh.com/pre-release/ui/kibana/wazuh_kibana-${WAZUH_VERSION}_${ELK_VERSION}-${UI_REVISION}.zip > /dev/null || ( echo "Error version ${WAZUH_VERSION}-${ELK_VERSION} not supported." && exit 1 )
         else
-            logger "Error, repository value must take 'prod' or 'dev' value."
-            echo "Error, repository value must take 'prod' or 'dev' value."
+            logger "Error, repository value must take 'prod' (production) or 'dev' (development) value."
+            echo "Error, repository value must take 'prod' (production) or 'dev' (development) value."
             exit
         fi
     else 
@@ -158,7 +155,6 @@ check_version() {
 
 main() {
 
-    export DIRECTORY="/var/ossec"
     while [ -n "$1" ]; do
         case $1 in
             "-h" | "--help")
@@ -184,18 +180,6 @@ main() {
             else
                 logger "ERROR Need opendistro version."
                 echo "ERROR Need opendistro version."
-                help 1
-            fi
-            shift 2
-        ;;
-
-        "-p" | "--package")
-            if [ -n "$2" ]; then
-                export PACKAGE_VERSION="$2"
-                HAVE_PACKAGE_VERSION=true
-            else
-                logger "ERROR Need package version (4.1, 4.2, ...)"
-                echo "ERROR Need package version (4.1, 4.2, ...)"
                 help 1
             fi
             shift 2
@@ -236,17 +220,6 @@ main() {
             shift 2
         ;;
 
-        "-b"|"--branch")
-            if [ -n "$2" ]; then
-                BRANCH="$2"
-                shift 2
-            else
-                logger "ERROR: Need branch to build."
-                echo "ERROR: Need branch to build."
-                help 1
-            fi
-        ;;
-
         "-s"|"--store-path")
             if [ -n "$2" ]; then
                 OUTPUT_DIR="$2"
@@ -277,18 +250,28 @@ main() {
     if [ -z "${CHECKSUM_DIR}" ]; then
         CHECKSUM_DIR="${OUTPUT_DIR}"
     fi
-    if  [ "${HAVE_PACKAGE_VERSION}" = true ] && [ "${HAVE_VERSION}" = true ] && [ "${HAVE_ELK_VERSION}" = true ] && [ "${HAVE_PACKAGES_REPOSITORY}" = true ] && [ "${HAVE_OPENDISTRO_VERSION}" = true ]; then
+    if  [ "${HAVE_VERSION}" = true ] && [ "${HAVE_ELK_VERSION}" = true ] && [ "${HAVE_PACKAGES_REPOSITORY}" = true ] && [ "${HAVE_OPENDISTRO_VERSION}" = true ]; then
+       
         export UI_REVISION="${UI_REVISION}"
         check_version
+
         OVA_VERSION="${WAZUH_VERSION}_${OPENDISTRO_VERSION}"
-        logger "Version to build: ${WAZUH_VERSION}-${OPENDISTRO_VERSION} with ${PACKAGES_REPOSITORY} repository and ${BRANCH} branch of package ${PACKAGE_VERSION}"
-        echo "Version to build: ${WAZUH_VERSION}-${OPENDISTRO_VERSION} with ${PACKAGES_REPOSITORY} repository and ${BRANCH} branch of package ${PACKAGE_VERSION}"
-        
+        [[ ${PACKAGES_REPOSITORY} = "prod" ]] && REPO="production" || REPO="development"
+        logger "Version to build: ${WAZUH_VERSION}-${OPENDISTRO_VERSION} with ${REPO} repository"
+        echo "Version to build: ${WAZUH_VERSION}-${OPENDISTRO_VERSION} with ${REPO} repository"
+        # Get the position of the last . from the wazuh version to get the branch 
+        POS=$(echo ${WAZUH_VERSION} | grep -aob '\.' | tail -1) 
+        POS=${POS:0:1}
+
+        # Get the branch of wazuh version
+        BRANCH="${WAZUH_VERSION:0:${POS}}"
+        export BRANCH
+
         # Build OVA file (no standard)
-        build_ova ${WAZUH_VERSION} ${OVA_VERSION} ${PACKAGE_VERSION}
+        build_ova ${WAZUH_VERSION} ${OVA_VERSION} ${BRANCH}
 
         # Standarize OVA
-        ./setOVADefault.sh "${scriptpath}" "${OUTPUT_DIR}/${OVA_VM}" "${OUTPUT_DIR}/${OVA_VM}" "${scriptpath}/wazuh_ovf_template" "${WAZUH_VERSION}" "${OPENDISTRO_VERSION}"
+        bash setOVADefault.sh "${scriptpath}" "${OUTPUT_DIR}/${OVA_VM}" "${OUTPUT_DIR}/${OVA_VM}" "${scriptpath}/wazuh_ovf_template" "${WAZUH_VERSION}" "${OPENDISTRO_VERSION}"
         
         logger "Process finished"
         echo "Process finished"
