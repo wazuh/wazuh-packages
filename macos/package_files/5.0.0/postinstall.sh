9bba88c6acc4c5092c1c016566b74441ce9e5a50
VictorMorenoJimenez <victormoreno.tech@protonmail.com>
2021-04-29 13:30:02 +0200

    Merge 5.0.0 changes from master to 5.0-dev

--- macos/package_files/5.0.0/postinstall.sh
+++ macos/package_files/5.0.0/postinstall.sh
@@ -8,8 +8,8 @@
 #  starting at 600 puts this in user space
 # -Added lines to append the ossec users to the group ossec
 #  so the the list GroupMembership works properly
-GROUP="ossec"
-USER="ossec"
+GROUP="wazuh"
+USER="wazuh"
 DIR="/Library/Ossec"
 INSTALLATION_SCRIPTS_DIR="${DIR}/packages_files/agent_installation_scripts"
 SCA_BASE_DIR="${INSTALLATION_SCRIPTS_DIR}/sca"
@@ -74,7 +74,7 @@ launchctl unsetenv WAZUH_RESTART
 
 if [ "${upgrade}" = "false" ]; then
     ${INSTALLATION_SCRIPTS_DIR}/gen_ossec.sh conf agent ${DIST_NAME} ${DIST_VER}.${DIST_SUBVER} ${DIR} > ${DIR}/etc/ossec.conf
-    chown root:ossec ${DIR}/etc/ossec.conf
+    chown root:wazuh ${DIR}/etc/ossec.conf
     chmod 0640 ${DIR}/etc/ossec.conf
 fi
 
@@ -113,6 +113,25 @@ ${INSTALLATION_SCRIPTS_DIR}/src/init/darwin-init.sh ${DIR}
 
 # Remove temporary directory
 rm -rf ${DIR}/packages_files
+
+# Remove old ossec user and group if exists and change ownwership of files
+
+if [[ $(dscl . -read /Groups/ossec) ]]; then
+  find %{_localstatedir} -group ossec -user root -exec chown root:wazuh {} \ > /dev/null 2>&1 || true
+  if [ $(dscl . -read /Users/ossec) ]]; then
+    find %{_localstatedir} -group ossec -user ossec -exec chown wazuh:wazuh {} \ > /dev/null 2>&1 || true
+    sudo /usr/bin/dscl . -delete "/Users/ossec"
+  fi
+  if [ $(dscl . -read /Users/ossecm) ]]; then
+    find %{_localstatedir} -group ossec -user ossecm -exec chown wazuh:wazuh {} \ > /dev/null 2>&1 || true
+    sudo /usr/bin/dscl . -delete "/Users/ossecm"
+  fi
+  if [ $(dscl . -read /Users/ossecr) ]]; then
+    find %{_localstatedir} -group ossec -user ossecr -exec chown wazuh:wazuh {} \ > /dev/null 2>&1 || true
+    sudo /usr/bin/dscl . -delete "/Users/ossecr"
+  fi
+  sudo /usr/bin/dscl . -delete "/Groups/wazuh"
+fi
 
 if ${upgrade} && ${restart}; then
     ${DIR}/bin/wazuh-control restart
--- solaris/solaris10/uninstall.sh
+++ solaris/solaris10/uninstall.sh
@@ -19,7 +19,7 @@ rm -rf /etc/rc2.d/S97wazuh-agent
 rm -rf /etc/rc3.d/S97wazuh-agent
 
 ## Remove User and Groups
-userdel ossec 2> /dev/null
-groupdel ossec 2> /dev/null
+userdel wazuh 2> /dev/null
+groupdel wazuh 2> /dev/null
 
 exit 0
