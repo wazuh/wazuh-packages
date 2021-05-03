4b21d73f117d870df62e4f574c9506230702c5ee
rauldpm <raul.delpozo@wazuh.com>
2021-05-03 15:05:57 +0200

    Reorganized structure, added cleaning tasks, disk optimization removed, fixed sed commands

--- ova/provision.sh
+++ ova/provision.sh
@@ -12,6 +12,83 @@ INSTALLER="all-in-one-installation.sh"
 # Display dev/prod
 echo "Using ${PACKAGES_REPOSITORY} packages"
 
+# Create user wazuh - Ignore password error
+adduser wazuh
+yes wazuh | sudo passwd wazuh
+
+# Grant sudo privileges to user
+gpasswd -a wazuh wheel
+
+# Set Hostname
+hostname wazuhmanager
+
+# Ssh config
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+
+# Download unattended installer
+curl -so ${INSTALLER} https://raw.githubusercontent.com/wazuh/wazuh-documentation/${BRANCH}/resources/open-distro/unattended-installation/${INSTALLER} 
+
+# Get currents version values of installer
+ACTUAL_W=$(less ${INSTALLER} | grep "WAZUH_VER=")
+ACTUAL_O=$(less ${INSTALLER} | grep "OD_VER=")
+ACTUAL_E=$(less ${INSTALLER} | grep "ELK_VER=")
+
+# Change specified versions in unattended installer
+sed -i "s/${ACTUAL_W}/WAZUH_VER=\"${WAZUH_VERSION}\"/g" ${INSTALLER}
+sed -i "s/${ACTUAL_O}/OD_VER=\"${OPENDISTRO_VERSION}\"/g" ${INSTALLER}
+sed -i "s/${ACTUAL_E}/ELK_VER=\"${ELK_VERSION}\"/g" ${INSTALLER}
+
+# Execute unattended installer
+sh ${INSTALLER}
+
+# Remove installer
+rm ${INSTALLER}
+
+# Stop services and enable manager
+systemctl stop kibana filebeat elasticsearch wazuh-manager
+systemctl enable wazuh-manager
+
+# Disable all host modules
+sed -i "s/<disabled>no/<disabled>yes/g" /var/ossec/etc/ossec.conf
+sed -i "s/<enabled>yes/<enabled>no/g" /var/ossec/etc/ossec.conf
+
+# Disable /var/log/ files and commands monitoring
+sed -i "s/<localfile>/<!--localfile>/g" /var/ossec/etc/ossec.conf
+sed -i "s/<\/localfile>/<\/localfile-->/g" /var/ossec/etc/ossec.conf
+
+# Enable rule_test and ossec_auth modules
+sed -i '/<auth>/ {N; s/<auth>.*yes/<auth>\n\ \ \ \ <disabled>no/g}' /var/ossec/etc/ossec.conf
+
+# Custom Login Page
+# Edit window title
+sed -i "s/null, \"Elastic\"/null, \"Wazuh\"/g" /usr/share/kibana/src/core/server/rendering/views/template.js
+
+# Download custom files (background, logo and template)
+curl -so /vagrant/custom_welcome.tar.gz https://wazuh-demo.s3-us-west-1.amazonaws.com/custom_welcome_opendistro_docker.tar.gz
+tar -xf /vagrant/custom_welcome.tar.gz -C /vagrant
+
+# Copy necesaries files
+cp /vagrant/custom_welcome/wazuh_logo_circle.svg /usr/share/kibana/src/core/server/core_app/assets/
+cp /vagrant/custom_welcome/wazuh_wazuh_bg.svg /usr/share/kibana/src/core/server/core_app/assets/
+cp /vagrant/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
+
+# Add custom configuration to css
+less /vagrant/assets/customWelcomeKibana.txt >> /usr/share/kibana/src/core/server/core_app/assets/legacy_light_theme.css
+
+# Get actual RAM of machine and split it in half
+ram=$(( $(free -m | awk '/^Mem:/{print $2}') / 2 ))
+
+# Change de jvm.options with the new RAM use
+sed -i "s/^-Xms[0-9]\+[gm]/-Xms${ram}m/" /etc/elasticsearch/jvm.options
+sed -i "s/^-Xmx[0-9]\+[gm]/-Xmx${ram}m/" /etc/elasticsearch/jvm.options
+
+# Remove vagrant user - by default this script runs in /home/vagrant 
+userdel -rf vagrant
+
+# Remove vagrant shared folder
+rm -rf /vagrant
+
 # OVA Welcome message
 cat > /etc/issue <<EOF
 
@@ -19,8 +96,8 @@ Welcome to the Wazuh OVA version
 Wazuh - ${WAZUH_VERSION}
 Open Distro for Elasticsearch - ${OPENDISTRO_VERSION}
 ELK - ${ELK_VERSION}
-Access the Wazuh Web Interface at https://\4{eth0} with admin/wazuh
-Login with User: wazuh - Password: wazuh
+Access the Wazuh Web Interface at https://\4{eth0} with admin/admin
+Access this machine with wazuh/wazuh
 Thank you for using Wazuh!
 
 EOF
@@ -60,96 +137,14 @@ WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW.
 
 EOF
 
+# Clean cache
+yum clean all
 
-# Create user wazuh - Ignore password error
-adduser wazuh
-yes wazuh | sudo passwd wazuh
+# Remove data from /var/ossec/logs/
+find /var/ossec/logs/ -type f -exec sh -c ': > "$1"' - {} \;
 
-# Grant sudo privileges to user
-gpasswd -a wazuh wheel
+# Remove data from /var/log/ files
+find /var/log/ -type f -exec sh -c ': > "$1"' - {} \;
 
-# Set Hostname
-hostname wazuhmanager
-
-# Ssh config
-sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-
-# Download unattended installer
-curl -so ${INSTALLER} https://raw.githubusercontent.com/wazuh/wazuh-documentation/${BRANCH}/resources/open-distro/unattended-installation/${INSTALLER} 
-
-# Get currents version values of installer
-ACTUAL_W=$(less ${INSTALLER} | grep "WAZUH_VER=")
-ACTUAL_O=$(less ${INSTALLER} | grep "OD_VER=")
-ACTUAL_E=$(less ${INSTALLER} | grep "ELK_VER=")
-
-# Change specified versions in unattended installer
-sed -i "s/${ACTUAL_W}/WAZUH_VER=\"${WAZUH_VERSION}\"/g" ${INSTALLER}
-sed -i "s/${ACTUAL_O}/OD_VER=\"${OPENDISTRO_VERSION}\"/g" ${INSTALLER}
-sed -i "s/${ACTUAL_E}/ELK_VER=\"${ELK_VERSION}\"/g" ${INSTALLER}
-
-# Execute unattended installer
-sh ${INSTALLER}
-
-# Remove installer
-rm ${INSTALLER}
-
-# Change admin password for kibana
-curl -so /vagrant/wazuh-passwords-tool.sh https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.1/resources/open-distro/tools/wazuh-passwords-tool.sh
-bash /vagrant/wazuh-passwords-tool.sh -u admin -p wazuh
-sed -i "s/password: \"wazuh\"/password: \"wazuh\"/g" /etc/filebeat/filebeat.yml
-
-# Stop services and enable manager
-systemctl stop kibana filebeat elasticsearch
-systemctl enable wazuh-manager
-
-# Disable all alerts on host (manager)
-sed -i "s/<disabled>no/<disabled>yes/g" /var/ossec/etc/ossec.conf
-sed -i "s/<enabled>yes/<enabled>no/g" /var/ossec/etc/ossec.conf
-
-# Enable rule_test and ossec_auth
-sed -i 'N; s/^[]*<rule_set>\n[ ]*<enabled>no/\ \ \ \ <rule_set>\n\ \ \ \ \ \ <enabled>yes/g' /var/ossec/etc/ossec.conf
-sed -i 'N; s/^[]*<auth>\n[ ]*<disabled>yes/\ \ \ \ <auth>\n\ \ \ \ \ \ <disabled>no/g' /var/ossec/etc/ossec.conf
-
-# Custom Welcome Page
-# Edit window title
-sed -i "s/null, \"Elastic\"/null, \"Wazuh\"/g" /usr/share/kibana/src/core/server/rendering/views/template.js
-
-# Download custom files
-curl -so /vagrant/custom_welcome.tar.gz https://wazuh-demo.s3-us-west-1.amazonaws.com/custom_welcome_opendistro_docker.tar.gz
-tar -xf /vagrant/custom_welcome.tar.gz -C /vagrant
-
-# Copy necesaries files
-cp /vagrant/custom_welcome/wazuh_logo_circle.svg /usr/share/kibana/src/core/server/core_app/assets/
-cp /vagrant/custom_welcome/wazuh_wazuh_bg.svg /usr/share/kibana/src/core/server/core_app/assets/
-cp /vagrant/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
-
-# Edit the ui
-less /vagrant/assets/customWelcomeKibana.txt >> /usr/share/kibana/src/core/server/core_app/assets/legacy_light_theme.css
-
-# Get actual RAM of machine
-ram_gb=$(free -m | awk '/^Mem:/{print $2}')
-
-# Split RAM in half
-ram=$(( $(free -m | awk '/^Mem:/{print $2}') / 2 ))
-
-# Change de jvm.options with the new RAM use
-sed -i "s/-Xms[0-9]\+[gm]/-Xms${ram}m/" "/etc/elasticsearch/jvm.options"
-sed -i "s/-Xmx[0-9]\+[gm]/-Xmx${ram}m/" "/etc/elasticsearch/jvm.options"
-
-# Remove vagrant user - by default this script runs in /home/vagrant 
-userdel -rf vagrant
-
-# Remove vagrant shared folder
-rm -rf /vagrant
-
-# Disk optimization
-#touch /var/tmp/emptyfile
-#dd if=/dev/zero of=/var/tmp/emptyfile bs=4096k | true
-#rm /var/tmp/emptyfile
-
-# Clean history
+# Delete history
 history -c
-
-# Temporary fix for content loss in wazuh kibana plugin files
-reboot now
