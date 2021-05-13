b439b2a5b567485fb12c8f7826b2e90a8b4a095c
rauldpm <raul.delpozo@wazuh.com>
2021-05-13 17:52:30 +0200

    Fix identation and remove the forced debug mode

--- ova/assets/steps.sh
+++ ova/assets/steps.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
 # Set debug mode
-# [[ ${DEBUG} = "yes" ]] && set -exf || set -ef
-set -exf
+[[ ${DEBUG} = "yes" ]] && set -exf || set -ef
 
 # Edit system config
 configSystem() {
 
-    # Change root password (root:wazuh)
-    sed -i "s/root:.*:/root:\$1\$pNjjEA7K\$USjdNwjfh7A\.vHCf8suK41::0:99999:7:::/g" /etc/shadow 
+  # Change root password (root:wazuh)
+  sed -i "s/root:.*:/root:\$1\$pNjjEA7K\$USjdNwjfh7A\.vHCf8suK41::0:99999:7:::/g" /etc/shadow 
 
+  # Add user wazuh (wazuh:wazuh)
+  adduser wazuh
+  sed -i "s/wazuh:!!/wazuh:\$1\$pNjjEA7K\$USjdNwjfh7A\.vHCf8suK41/g" /etc/shadow 
 
-    # Add user wazuh (wazuh:wazuh)
-    adduser wazuh
-    sed -i "s/wazuh:!!/wazuh:\$1\$pNjjEA7K\$USjdNwjfh7A\.vHCf8suK41/g" /etc/shadow 
+  # Grant sudo privileges to user
+  gpasswd -a wazuh wheel
 
-    # Grant sudo privileges to user
-    gpasswd -a wazuh wheel
+  # Set Hostname
+  hostname wazuhmanager
 
-    # Set Hostname
-    hostname wazuhmanager
+  # Ssh config
+  sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+  echo "PermitRootLogin no" >> /etc/ssh/sshd_config
 
-    # Ssh config
-    sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
-    echo "PermitRootLogin no" >> /etc/ssh/sshd_config
-
-    # OVA Welcome message
-    cat > /etc/issue <<EOF
+  # OVA Welcome message
+  cat > /etc/issue <<EOF
 
 Welcome to the Wazuh OVA version 
 Wazuh - ${WAZUH_VERSION}
@@ -38,8 +36,8 @@ Thank you for using Wazuh!
 
 EOF
 
-    # User Welcome message
-    cat > /etc/motd <<EOF
+  # User Welcome message
+  cat > /etc/motd <<EOF
 
               W.                   W.
              WWW.                 WWW.
@@ -78,106 +76,106 @@ EOF
 # Edit unnatended installer
 preInstall() {
 
-    # Set debug mode
-    if [ "${DEBUG}" == "yes" ]; then
-      sed -i "s/\#\!\/bin\/bash/\#\!\/bin\/bash\nset -x/g" ${INSTALLER}
-    fi
+  # Set debug mode
+  if [ "${DEBUG}" == "yes" ]; then
+    sed -i "s/\#\!\/bin\/bash/\#\!\/bin\/bash\nset -x/g" ${INSTALLER}
+  fi
 
-    # Get currents version values of installer
-    CURRENT_W=$(less ${INSTALLER} | grep "WAZUH_VER=")
-    CURRENT_O=$(less ${INSTALLER} | grep "OD_VER=")
-    CURRENT_E=$(less ${INSTALLER} | grep "ELK_VER=")
+  # Get currents version values of installer
+  CURRENT_W=$(less ${INSTALLER} | grep "WAZUH_VER=")
+  CURRENT_O=$(less ${INSTALLER} | grep "OD_VER=")
+  CURRENT_E=$(less ${INSTALLER} | grep "ELK_VER=")
 
-    # Change wazuh and documentation repository branch
-    sed -i "s/uh\/[0-9]\+\.[0-9]\+\/ex/uh\/${BRANCH}\/ex/g" ${INSTALLER}
-    sed -i "s/on\/[0-9]\+\.[0-9]\+\/re/on\/${BRANCHDOC}\/re/g" ${INSTALLER}
+  # Change wazuh and documentation repository branch
+  sed -i "s/uh\/[0-9]\+\.[0-9]\+\/ex/uh\/${BRANCH}\/ex/g" ${INSTALLER}
+  sed -i "s/on\/[0-9]\+\.[0-9]\+\/re/on\/${BRANCHDOC}\/re/g" ${INSTALLER}
 
-    # Change versions
-    sed -i "s/${CURRENT_W}/WAZUH_VER=\"${WAZUH_VERSION}\"/g" ${INSTALLER}
-    sed -i "s/${CURRENT_O}/OD_VER=\"${OPENDISTRO_VERSION}\"/g" ${INSTALLER}
-    sed -i "s/${CURRENT_E}/ELK_VER=\"${ELK_VERSION}\"/g" ${INSTALLER}
+  # Change versions
+  sed -i "s/${CURRENT_W}/WAZUH_VER=\"${WAZUH_VERSION}\"/g" ${INSTALLER}
+  sed -i "s/${CURRENT_O}/OD_VER=\"${OPENDISTRO_VERSION}\"/g" ${INSTALLER}
+  sed -i "s/${CURRENT_E}/ELK_VER=\"${ELK_VERSION}\"/g" ${INSTALLER}
 
-    # Change repository if dev is specified
-    if [ "${PACKAGES_REPOSITORY}" = "dev" ]; then
-        sed -i "s/\[wazuh\]/\[wazuh_pre_release\]/g" ${INSTALLER}
-        sed -i "s/ngpgkey\=https\:\/\/packages\.wazuh\.com/ngpgkey\=https\:\/\/packages\-dev\.wazuh\.com/g" ${INSTALLER}
-        sed -i "s/baseurl\=https\:\/\/packages\.wazuh\.com\/4\.x/baseurl\=https\:\/\/packages\-dev\.wazuh\.com\/pre\-release/g" ${INSTALLER}
-        sed -i "s/https\:\/\/packages\.wazuh\.com\/4\.x\/ui\/kibana/https\:\/\/packages\-dev\.wazuh\.com\/pre\-release\/ui\/kibana/g" ${INSTALLER}
-        sed -i "s/wazuh_kibana-[0-9\.]\+_[0-9\.]\+/wazuh_kibana-${WAZUH_VERSION}_${ELK_VERSION}/g" ${INSTALLER}
-    fi
+  # Change repository if dev is specified
+  if [ "${PACKAGES_REPOSITORY}" = "dev" ]; then
+      sed -i "s/\[wazuh\]/\[wazuh_pre_release\]/g" ${INSTALLER}
+      sed -i "s/ngpgkey\=https\:\/\/packages\.wazuh\.com/ngpgkey\=https\:\/\/packages\-dev\.wazuh\.com/g" ${INSTALLER}
+      sed -i "s/baseurl\=https\:\/\/packages\.wazuh\.com\/4\.x/baseurl\=https\:\/\/packages\-dev\.wazuh\.com\/pre\-release/g" ${INSTALLER}
+      sed -i "s/https\:\/\/packages\.wazuh\.com\/4\.x\/ui\/kibana/https\:\/\/packages\-dev\.wazuh\.com\/pre\-release\/ui\/kibana/g" ${INSTALLER}
+      sed -i "s/wazuh_kibana-[0-9\.]\+_[0-9\.]\+/wazuh_kibana-${WAZUH_VERSION}_${ELK_VERSION}/g" ${INSTALLER}
+  fi
 
-    # Add Kibana custom user wazuh
-    PATTERN="eval \"rm \/etc\/elasticsearch\/e"
-    HASH="\\\$2y\\\$12\\\$qCvlv3y4\\\.i8nX6wUZOepROVhTWI36H8nH2gxwShHcpIzf0yV1J30K"  # password: wazuh
-    FILE_PATH="\/usr\/share\/elasticsearch\/plugins\/opendistro_security\/securityconfig"
-    sed -i "s/${PATTERN}/sed -i \'\/admin:\/ {N; s\/admin.*\/wazuh:\\\n  hash: \"${HASH}\"\/g}\' ${FILE_PATH}\/internal_users\.yml\n        ${PATTERN}/g" ${INSTALLER}
-    
-    # Set backend_role for wazuh user with all_access
-    sed -i "s/${PATTERN}/sed -i \'s\/\"admin\"\/\"wazuh\"\/g\' ${FILE_PATH}\/roles_mapping\.yml\n        ${PATTERN}/g" ${INSTALLER}
-    sed -i "s/${PATTERN}/sed -i \'s\/\"admin\"\/\"wazuh\"\/g\' ${FILE_PATH}\/internal_users\.yml\n        ${PATTERN}/g" ${INSTALLER}
+  # Add Kibana custom user wazuh
+  PATTERN="eval \"rm \/etc\/elasticsearch\/e"
+  HASH="\\\$2y\\\$12\\\$qCvlv3y4\\\.i8nX6wUZOepROVhTWI36H8nH2gxwShHcpIzf0yV1J30K"  # password: wazuh
+  FILE_PATH="\/usr\/share\/elasticsearch\/plugins\/opendistro_security\/securityconfig"
+  sed -i "s/${PATTERN}/sed -i \'\/admin:\/ {N; s\/admin.*\/wazuh:\\\n  hash: \"${HASH}\"\/g}\' ${FILE_PATH}\/internal_users\.yml\n        ${PATTERN}/g" ${INSTALLER}
+  
+  # Set backend_role for wazuh user with all_access
+  sed -i "s/${PATTERN}/sed -i \'s\/\"admin\"\/\"wazuh\"\/g\' ${FILE_PATH}\/roles_mapping\.yml\n        ${PATTERN}/g" ${INSTALLER}
+  sed -i "s/${PATTERN}/sed -i \'s\/\"admin\"\/\"wazuh\"\/g\' ${FILE_PATH}\/internal_users\.yml\n        ${PATTERN}/g" ${INSTALLER}
 
-    # Change user:password in curls
-    sed -i "s/admin:admin/wazuh:wazuh/g" ${INSTALLER}
+  # Change user:password in curls
+  sed -i "s/admin:admin/wazuh:wazuh/g" ${INSTALLER}
 
-    # Change user:password in filebeat.yml
-    PATTERN="eval \"curl -so \/etc\/filebeat\/wazuh-template"
-    sed -i "s/${PATTERN}/sed -i \"s\/admin\/wazuh\/g\" \/etc\/filebeat\/filebeat\.yml\n        ${PATTERN}/g" ${INSTALLER}
+  # Change user:password in filebeat.yml
+  PATTERN="eval \"curl -so \/etc\/filebeat\/wazuh-template"
+  sed -i "s/${PATTERN}/sed -i \"s\/admin\/wazuh\/g\" \/etc\/filebeat\/filebeat\.yml\n        ${PATTERN}/g" ${INSTALLER}
 
-    # Change UI_REVISION in installer
-    sed -i "s/-1\.zip/-${UI_REVISION}.zip/g" ${INSTALLER}
+  # Change UI_REVISION in installer
+  sed -i "s/-1\.zip/-${UI_REVISION}.zip/g" ${INSTALLER}
 
 }
 
 # Edit wazuh installation
 postInstall() {
 
-    # Custom Login Page
-    # Edit window title
-    sed -i "s/null, \"Elastic\"/null, \"Wazuh\"/g" /usr/share/kibana/src/core/server/rendering/views/template.js
+  # Custom Login Page
+  # Edit window title
+  sed -i "s/null, \"Elastic\"/null, \"Wazuh\"/g" /usr/share/kibana/src/core/server/rendering/views/template.js
 
-    # Download custom files (background, logo and template)
-    curl -so ${CURRENT_PATH}/custom_welcome.tar.gz https://wazuh-demo.s3-us-west-1.amazonaws.com/custom_welcome_opendistro_docker.tar.gz
-    tar -xf ${CURRENT_PATH}/custom_welcome.tar.gz -C ${CURRENT_PATH}
+  # Download custom files (background, logo and template)
+  curl -so ${CURRENT_PATH}/custom_welcome.tar.gz https://wazuh-demo.s3-us-west-1.amazonaws.com/custom_welcome_opendistro_docker.tar.gz
+  tar -xf ${CURRENT_PATH}/custom_welcome.tar.gz -C ${CURRENT_PATH}
 
-    # Copy necesaries files
-    cp ${CURRENT_PATH}/custom_welcome/wazuh_logo_circle.svg /usr/share/kibana/src/core/server/core_app/assets/
-    cp ${CURRENT_PATH}/custom_welcome/wazuh_wazuh_bg.svg /usr/share/kibana/src/core/server/core_app/assets/
-    cp ${CURRENT_PATH}/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
+  # Copy necesaries files
+  cp ${CURRENT_PATH}/custom_welcome/wazuh_logo_circle.svg /usr/share/kibana/src/core/server/core_app/assets/
+  cp ${CURRENT_PATH}/custom_welcome/wazuh_wazuh_bg.svg /usr/share/kibana/src/core/server/core_app/assets/
+  cp ${CURRENT_PATH}/custom_welcome/template.js.hbs /usr/share/kibana/src/legacy/ui/ui_render/bootstrap/template.js.hbs
 
-    # Add custom configuration to css
-    less ${CURRENT_PATH}/assets/customWelcomeKibana.css >> /usr/share/kibana/src/core/server/core_app/assets/legacy_light_theme.css
+  # Add custom configuration to css
+  less ${CURRENT_PATH}/assets/customWelcomeKibana.css >> /usr/share/kibana/src/core/server/core_app/assets/legacy_light_theme.css
 
-    # Get actual RAM of machine and split it in half
-    ram=$(( $(free -m | awk '/^Mem:/{print $2}') / 2 ))
+  # Get actual RAM of machine and split it in half
+  ram=$(( $(free -m | awk '/^Mem:/{print $2}') / 2 ))
 
-    # Change de jvm.options with the new RAM use
-    sed -i "s/^-Xms[0-9]\+[gm]/-Xms${ram}m/" /etc/elasticsearch/jvm.options
-    sed -i "s/^-Xmx[0-9]\+[gm]/-Xmx${ram}m/" /etc/elasticsearch/jvm.options
+  # Change de jvm.options with the new RAM use
+  sed -i "s/^-Xms[0-9]\+[gm]/-Xms${ram}m/" /etc/elasticsearch/jvm.options
+  sed -i "s/^-Xmx[0-9]\+[gm]/-Xmx${ram}m/" /etc/elasticsearch/jvm.options
 
 }
 
 clean() {
 
-    # Remove installer
-    rm ${INSTALLER}
+  # Remove installer
+  rm ${INSTALLER}
 
-    # Remove synced vagrant content
-    rm -rf ${CURRENT_PATH}/*
+  # Remove synced vagrant content
+  rm -rf ${CURRENT_PATH}/*
 
-    # Clean cache
-    yum clean all
+  # Clean cache
+  yum clean all
 
-    # Remove data from /var/ossec/logs/
-    cd / 
-    find /var/ossec/logs/ -type f -exec sh -c ': > "$1"' - {} \;
+  # Remove data from /var/ossec/logs/
+  cd / 
+  find /var/ossec/logs/ -type f -exec sh -c ': > "$1"' - {} \;
 
-    # Remove data from /var/log/ files
-    find /var/log/ -type f -exec sh -c ': > "$1"' - {} \;
+  # Remove data from /var/log/ files
+  find /var/log/ -type f -exec sh -c ': > "$1"' - {} \;
 
-    # Remove demo script
-    rm securityadmin_demo.sh
+  # Remove demo script
+  rm securityadmin_demo.sh
 
-    # Delete history
-    history -c
+  # Delete history
+  history -c
 
 }
