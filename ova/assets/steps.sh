e9d0c9ae7630bc9f187bf68654524c5dce253991
rauldpm <raul.delpozo@wazuh.com>
2021-05-13 16:23:42 +0200

    Remove -f option in the installer script due to symslink error

--- ova/assets/steps.sh
+++ ova/assets/steps.sh
@@ -80,9 +80,9 @@ preInstall() {
 
     # Set debug mode
     if [ "${DEBUG}" == "yes" ]; then
-      sed -i "s/\#\!\/bin\/bash/\#\!\/bin\/bash\nset -xf/g" ${INSTALLER}
+      sed -i "s/\#\!\/bin\/bash/\#\!\/bin\/bash\nset -x/g" ${INSTALLER}
     fi
-    
+
     # Get currents version values of installer
     CURRENT_W=$(less ${INSTALLER} | grep "WAZUH_VER=")
     CURRENT_O=$(less ${INSTALLER} | grep "OD_VER=")
