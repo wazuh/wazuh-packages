7dfacdb7389e7f082bda9ebb7540ab6d7a08c132
rauldpm <raul.delpozo@wazuh.com>
2021-05-18 17:50:44 +0200

    Added debug mode, created service to remove vagrant

--- ova/assets/clean.sh
+++ ova/assets/clean.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 
+DEBUG=$1
+[[ ${DEBUG} = "yes" ]] && set -ex || set -e
+
+CURRENT_PATH="$( cd $(dirname $0) ; pwd -P )"
+
 # Stop services after reload
 systemctl stop wazuh-manager elasticsearch filebeat kibana
 
-# Check disk integrity
-fsck /dev/sda1
-
 # Remove everything related to vagrant
-userdel -rf vagrant
+mv ${CURRENT_PATH}/assets/removeVagrant.sh /home/wazuh/
+chmod 755 /home/wazuh/removeVagrant.sh
+
+cat > /etc/systemd/system/removeVagrant.service <<EOF
+[Unit]
+Description=Remove vagrant
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/bin/bash /home/wazuh/removeVagrant.sh
+Type=simple
+User=root
+Group=root
+WorkingDirectory=/home/wazuh
+Restart=always
+RestartSec=3
+EOF
+
+systemctl daemon-reload
+systemctl enable removeVagrant.service
+
+# Clear synced files
+rm -rf ${CURRENT_PATH}/* ${CURRENT_PATH}/.gitignore
 
 # Remove logs
-cd /
-find /var/log/ -type f -exec sh -c \': > \"\$1\"\' - {} \;
-find /var/ossec/logs/ -type f -exec sh -c \': > \"\$1\"\' - {} \;
+find /var/log/ -type f -exec sh -c ': > "$1"' - {} \;
+find /var/ossec/logs/ -type f -exec sh -c ': > "$1"' - {} \;
 
 # Clean history
 history -c
+
+# Apply cleaning changes
+shutdown -r now
