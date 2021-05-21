cc33177265995c42f4149d652a7e2774738cedb4
rauldpm <raul.delpozo@wazuh.com>
2021-05-21 15:08:22 +0200

    Assets folder reorganized

--- ova/assets/postProvision.sh
+++ ova/assets/postProvision.sh
@@ -4,20 +4,22 @@ DEBUG=$1
 [[ ${DEBUG} = "yes" ]] && set -ex || set -e
 
 CURRENT_PATH="$( cd $(dirname $0) ; pwd -P )"
+ASSETS_PATH="${CURRENT_PATH}/assets"
+CUSTOM_PATH="${ASSETS_PATH}/custom"
 
 # Stop services after reload
 systemctl stop wazuh-manager elasticsearch filebeat kibana
 
 # Set dinamic ram of vm
-mv ${CURRENT_PATH}/assets/automatic_set_ram.sh /etc/
+mv ${CUSTOM_PATH}/automatic_set_ram.sh /etc/
 chmod +x "/etc/automatic_set_ram.sh"
 echo "@reboot . /etc/automatic_set_ram.sh" >> cron
 crontab cron
 rm cron
 
 # Remove everything related to vagrant
-mv ${CURRENT_PATH}/assets/removeVagrant.service /etc/systemd/system/
-mv ${CURRENT_PATH}/assets/removeVagrant.sh /home/wazuh/
+mv ${CUSTOM_PATH}/removeVagrant.service /etc/systemd/system/
+mv ${CUSTOM_PATH}/removeVagrant.sh /home/wazuh/
 chmod 755 /home/wazuh/removeVagrant.sh
 systemctl daemon-reload
 systemctl enable removeVagrant.service
@@ -33,4 +35,4 @@ find /var/ossec/logs/ -type f -exec sh -c ': > "$1"' - {} \;
 history -c
 
 # Apply cleaning changes
-shutdown -r now
+shutdown -r now > /dev/null 2>&1
