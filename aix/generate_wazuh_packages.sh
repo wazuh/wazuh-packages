4c40bafcb3e475832c58496fef298fb027a70d02
rauldpm <raul.delpozo@wazuh.com>
2021-06-15 22:35:19 +0200

    Fix Perl to build with all aix versions

--- aix/generate_wazuh_packages.sh
+++ aix/generate_wazuh_packages.sh
@@ -220,8 +220,9 @@ build_environment() {
     $rpm http://www.oss4aix.org/download/RPMS/gcc/libstdc++-devel-6.3.0-1.aix7.2.ppc.rpm || true
   fi
 
+  build_perl
+
   if [[ "${aix_major}" = "5" ]]; then
-    build_perl
     build_libssh2
     build_curl
   fi
