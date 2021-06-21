4535f8c3ce80098255d482cd84a2eaa047a29f1e
rauldpm <raul.delpozo@wazuh.com>
2021-06-21 15:10:43 +0200

    Added default branch

--- wpk/run.sh
+++ wpk/run.sh
@@ -28,7 +28,7 @@ help() {
     echo "Usage: ${0} [OPTIONS]"
     echo "It is required to use -k or --aws-wpk-key, --aws-wpk-cert parameters"
     echo
-    echo "    -b,   --branch <branch>      [Required] Select Git branch or tag e.g. ${REFERENCE}"
+    echo "    -b,   --branch <branch>      [Required] Select Git branch or tag. By default: ${REFERENCE}"
     echo "    -o,   --output <name>        [Required] Name to the output package."
     echo "    -pn,  --package-name <name>  [Required for windows] Package name to pack on wpk."
     echo "    -r,   --revision <rev>       [Optional] Revision of the package. By default: 1."
