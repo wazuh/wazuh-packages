fb3a56c0f45d99cc451f73f2550658a890aac554
rauldpm <raul.delpozo@wazuh.com>
2021-06-29 17:04:50 +0200

    Fixed ssh PermitRootLogin yes in aws

--- ova/assets/steps.sh
+++ ova/assets/steps.sh
@@ -30,6 +30,9 @@ systemConfig() {
   gpasswd -a wazuh wheel
   hostname wazuh-manager
 
+  # AWS instance has this enabled
+  sed -i "s/PermitRootLogin yes/#PermitRootLogin yes/g" /etc/ssh/sshd_config
+
   # Ssh configuration
   sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
   echo "PermitRootLogin no" >> /etc/ssh/sshd_config
