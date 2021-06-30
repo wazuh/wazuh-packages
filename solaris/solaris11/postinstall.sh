0336c04898b93fe700329f6d547fb2afbed48af4
dfolcha <daniel.folch@wazuh.com>
2021-06-30 14:27:44 +0200

    Fix solaris 11 postinstall

--- solaris/solaris11/postinstall.sh
+++ solaris/solaris11/postinstall.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 # postinst script for wazuh-agent
 # Wazuh, Inc 2015-2020
+set -x
+install_path="<INSTALL_PATH>"
 
-if [ -d <INSTALL_PATH>/logs/ossec ]; then
-  rm -rf <INSTALL_PATH>/logs/wazuh
-  cp -rp <INSTALL_PATH>/logs/ossec <INSTALL_PATH>/logs/wazuh
-fi
-if [ -d <INSTALL_PATH>/queue/ossec ]; then
-  rm -rf <INSTALL_PATH>/queue/sockets
-  cp -rp <INSTALL_PATH>/queue/ossec <INSTALL_PATH>/queue/sockets
+if [ -d ${install_path}/logs/ossec ]; then
+  if [ -z "$(ls -A ${install_path}/logs/ossec)" ]; then
+    rm -rf ${install_path}/logs/ossec
+  else
+    rm -rf ${install_path}/logs/wazuh
+    mv ${install_path}/logs/ossec ${install_path}/logs/wazuh
+  fi
+fi  
+if [ -d ${install_path}/queue/ossec ]; then
+  if [ -z "$(ls -A ${install_path}/queue/ossec)" ]; then
+    rm -rf ${install_path}/queue/ossec
+  else
+    rm -rf ${install_path}/queue/sockets
+    mv ${install_path}/queue/ossec/ ${install_path}/queue/sockets
+  fi
 fi
 
 # Remove old ossec user and group if exists and change ownwership of files
