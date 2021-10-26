f5b76c59e71f741a4a24d9007861f187ae802a54
verd-x <verdx@riseup.net>
2021-10-26 10:53:54 +0200

    Added missing error when user given in password file doesn't exist

--- unattended_scripts/open-distro/tools/wazuh-passwords-tool.sh
+++ unattended_scripts/open-distro/tools/wazuh-passwords-tool.sh
@@ -168,12 +168,17 @@ User:
     fi
 
     if [ -n "${CHANGEALL}" ]; then
-        for i in "${!USERS[@]}"; do
-	    for j in "${!FILEUSERS[@]}"; do
+        for j in "${!FILEUSERS[@]}"; do
+	    supported=false
+	    for i in "${!USERS[@]}"; do
 	        if [[ ${USERS[i]} == ${FILEUSERS[j]} ]]; then
 		    PASSWORDS[i]=${FILEPASSWORDS[j]}
+		    supported=true
 		fi
 	    done
+            if [ $supported = false ]; then
+	        echo "Error: The given user ${FILEUSERS[j]} does not exist"
+	    fi
         done
     else
 	FINALUSERS=()
