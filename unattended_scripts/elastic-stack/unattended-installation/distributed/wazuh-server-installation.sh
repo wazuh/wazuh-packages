b2677c93e4432145f258a3fe854a3faf20d128ba
verd-x <verdx@riseup.net>
2021-11-04 18:20:54 +0100

    Added option to configure wazuh cluster using config file

--- unattended_scripts/elastic-stack/unattended-installation/distributed/wazuh-server-installation.sh
+++ unattended_scripts/elastic-stack/unattended-installation/distributed/wazuh-server-installation.sh
@@ -86,6 +86,7 @@ getHelp() {
    echo -e "\t-n    | --node-name Name of the node"
    echo -e "\t-p    | --elastic-password Elastic user password"
    echo -e "\t-d    | --debug Shows the complete installation output"
+   echo -e "\t-k    | --key <wazuh-cluster-key> Use this option as well as a wazuh_config.yml configuration file to automatically configure the wazuh cluster"
    echo -e "\t-h    | --help Shows help"
    exit 1 # Exit script after printing help
 
@@ -112,6 +113,12 @@ checkConfig() {
     else
         echo "No certificates file found."
         exit 1;
+    fi
+    if [ -n "${clusterkey}" ]; then
+        if [ ! -f ~/wazuh_config.yml ]; then
+            echo "No configuration file found for the wazuh cluster."
+            exit 1;
+        fi
     fi
 
 }
@@ -239,8 +246,32 @@ installWazuh() {
     else
         logger "Done"
     fi
-    startService "wazuh-manager"
 
+}
+
+configureWazuh() {
+    cn=$(awk '/cluster.name:/ {print $2}' ~/wazuh_config.yml)
+    nt=$(awk '/node.type:/ {print $2}' ~/wazuh_config.yml)
+    ma=$(awk '/master.address:/ {print $2}' ~/wazuh_config.yml)
+    ba=$(awk '/bind.address:/ {print $2}' ~/wazuh_config.yml)
+    port=$(awk '/port:/ {print $2}' ~/wazuh_config.yml)
+    hidden=$(awk '/hidden:/ {print $2}' ~/wazuh_config.yml)
+    disabled=$(awk '/disabled:/ {print $2}' ~/wazuh_config.yml)
+    lstart=$(grep -n "<cluster>" /var/ossec/etc/ossec.conf | cut -d : -f 1)
+    lend=$(grep -n "</cluster>" /var/ossec/etc/ossec.conf | cut -d : -f 1)
+
+    eval 'sed -i -e "${lstart},${lend}s/<name>.*<\/name>/<name>${cn}<\/name>/" \
+	-e "${lstart},${lend}s/<node_name>.*<\/node_name>/<node_name>${iname}<\/node_name>/" \
+	-e "${lstart},${lend}s/<node_type>.*<\/node_type>/<node_type>${nt}<\/node_type>/" \
+	-e "${lstart},${lend}s/<key>.*<\/key>/<key>${clusterkey}<\/key>/" \
+	-e "${lstart},${lend}s/<port>.*<\/port>/<port>${port}<\/port>/" \
+	-e "${lstart},${lend}s/<bind_addr>.*<\/bind_addr>/<bind_addr>${ba}<\/bind_addr>/" \
+	-e "${lstart},${lend}s/<node>.*<\/node>/<node>${ma}<\/node>/" \
+	-e "${lstart},${lend}s/<hidden>.*<\/hidden>/<hidden>${hidden}<\/hidden>/" \
+	-e "${lstart},${lend}s/<disabled>.*<\/disabled>/<disabled>${disabled}<\/disabled>/" \
+	/var/ossec/etc/ossec.conf'
+
+    startService "wazuh-manager"
 }
 
 ## Filebeat
@@ -384,6 +415,11 @@ main() {
                 d=1
                 shift 1
                 ;;
+            "-k"|"--key")
+                clusterkey=$2
+                shift
+                shift
+                ;;
             "-h"|"--help")
                 getHelp
                 ;;
@@ -422,6 +458,9 @@ main() {
         addElasticrepo
         addWazuhrepo
         installWazuh
+        if [ -n "${clusterkey}" ]; then
+            configureWazuh
+        fi
         installFilebeat iname
         configureFilebeat iname password
     else
