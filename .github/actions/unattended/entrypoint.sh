a287b6c8582c97fc91f584bc95d38cd6c8c0da74
Alberto R <alberpilot@gmail.com>
2021-11-17 17:27:18 +0100

    Added test to entrypoint

--- .github/actions/unattended/entrypoint.sh
+++ .github/actions/unattended/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-bash unattended_scripts/open-distro/unattended-installation/unattended-installation.sh
+bash unattended_scripts/open-distro/unattended-installation/unattended-installation.sh && cd ~ && /usr/testing/bin/pytest --tb=long /test_unattended.py -v
