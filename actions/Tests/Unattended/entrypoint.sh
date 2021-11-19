8e00590480b7e953d28e530b3718bca2b269b756
Alberto R <alberpilot@gmail.com>
2021-11-19 15:09:10 +0100

    Removed debug comments

--- actions/Tests/Unattended/entrypoint.sh
+++ actions/Tests/Unattended/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 bash unattended_scripts/open-distro/unattended-installation/unattended-installation.sh
+cat /var/log/wazuh-unattended-installation.log
 sleep 60
 cd ~
 /usr/testing/bin/pytest --tb=long /test_unattended.py -v
