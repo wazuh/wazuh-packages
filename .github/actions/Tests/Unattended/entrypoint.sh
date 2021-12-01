432cdadfd45620330821ee57215c504db72ba3e8
Alberto R <alberpilot@gmail.com>
2021-12-01 18:19:37 +0100

    Testing gh actions

--- .github/actions/Tests/Unattended/entrypoint.sh
+++ .github/actions/Tests/Unattended/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ${GITHUB_WORKSPACE}
-bash ./unattended_scripts/unattended_installation.sh -A -l
+cd ${GITHUB_WORKSPACE}/unattended_scripts
+bash ./unattended_installation.sh -A -l
 cat /var/log/wazuh-unattended-installation.log
 sleep 60
 cd ~
