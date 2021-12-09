b54a0000058d067d9ce86369dd1f64a6baa25cb6
verdx <verdx@riseup.net>
2021-12-09 20:09:42 +0100

    Added removed wazuh_install.sh

---
+++ unattended_scripts/wazuh_install.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+set -x
+## Package vars
+WAZUH_MAJOR="4.2"
+WAZUH_VER="4.2.5"
+WAZUH_REV="1"
+ELK_VER="7.10.2"
+ELKB_VER="7.12.1"
+OD_VER="1.13.2"
+OD_REV="1"
+WAZUH_KIB_PLUG_REV="1"
+
+## Links and paths to resources
+functions_path="install_functions/opendistro"
+config_path="config/opendistro"
+resources="https://s3.us-west-1.amazonaws.com/packages-dev.wazuh.com/resources/${WAZUH_MAJOR}"
+resources_functions="${resources}/${functions_path}"
+resources_config="${resources}/${config_path}"
+base_path="$(dirname $(realpath $0))"
+
+## Debug variable used during the installation
+debug='>> /var/log/wazuh-unattended-installation.log 2>&1'
+
+## Show script usage
+getHelp() {
+
+    echo ""
+    echo "Usage: $0 arguments"
+    echo -e "\t-A   | --AllInOne                      All-In-One installation"
+    echo -e "\t-w   | --wazuh                         Wazuh installation"
+    echo -e "\t-e   | --elasticsearch                 Elasticsearch installation"
+    echo -e "\t-k   | --kibana                        Kibana installation"
+    echo -e "\t-c   | --create-certificates           Create certificates from instances.yml file"
+    echo -e "\t-en  | --elastic-node-name             Name of the elastic node, used for distributed installations"
+    echo -e "\t-wn  | --wazuh-node-name               Name of the wazuh node, used for distributed installations"
+
+    echo -e "\t-wk  | --wazuh-key <wazuh-cluster-key> Use this option as well as a wazuh_cluster_config.yml configuration file to automatically configure the wazuh cluster when using a multi-node installation"
+    echo -e "\t-d   | --debug                         Shows the complete installation output"
+    echo -e "\t-i   | --ignore-health-check           Ignores the health-check"
+    echo -e "\t-l   | --local                         Use local files"
+    echo -e "\t-h   | --help                          Shows help"
+    exit 1 # Exit script after printing help
+
+}
+
+## Prints information
+logger() {
+
+    now=$(date +'%m/%d/%Y %H:%M:%S')
+    case $1 in 
+        "-e")
+            mtype="ERROR:"
+            message="$2"
+            ;;
+        "-w")
+            mtype="WARNING:"
+            message="$2"
+            ;;
+        *)
+            mtype="INFO:"
+            message="$1"
+            ;;
+    esac
+    echo $now $mtype $message | tee -a /var/log/wazuh-unattended-installation.log
+}
+
+importFunction() {
+    if [ -n "${local}" ]; then
+        if [ -f ${base_path}/$functions_path/$1 ]; then
+            eval "sed -i '/main @/d' ${base_path}/$functions_path/$1 ${debug}"
+            eval ". ${base_path}/$functions_path/$1"
+            echo 'main @' >> ${base_path}/$functions_path/$1
+        else 
+            error=1
+        fi
+    else
+        eval "curl -so /tmp/$1 $resources_functions/$1 ${debug}"
+        if [[ $? == 0 ]]; then
+            eval "sed -i '/main @/d' /tmp/$1 ${debug}"
+            eval ". /tmp/$1 ${debug}"
+            eval "rm -f /tmp/$1 ${debug}"
+        else
+            error=1 
+        fi
+    fi
+    if [[ ${error} == 1 ]]; then
+        logger -e "Unable to find resource $1. Exiting"
+        exit 1
+    fi
+}
+
+main() {
+    if [ "$EUID" -ne 0 ]; then
+        logger -e "This script must be run as root."
+        exit 1;
+    fi   
+
+    while [ -n "$1" ]
+    do
+        case "$1" in
+            "-A"|"--AllInOne")
+                AIO=1
+                shift 1
+                ;;
+            "-w"|"--wazuh")
+                wazuh=1
+                shift 1
+                ;;
+            "-e"|"--elasticsearch")
+                elastic=1
+                shift 1
+                ;;
+            "-k"|"--kibana")
+                kibana=1
+                shift 1
+                ;;
+            "-en"|"--elastic-node-name")
+                einame=$2
+                shift 2
+                ;;
+            "-wn"|"--wazuh-node-name")
+                winame=$2
+                shift 2
+                ;;
+
+            "-c"|"--create-certificates")
+                certificates=1
+                shift 1
+                ;;
+            "-i"|"--ignore-healthcheck")
+                ignore=1
+                shift 1
+                ;;
+            "-d"|"--debug")
+                debugEnabled=1
+                debug='2>&1 | tee -a /var/log/wazuh-unattended-installation.log'
+                shift 1
+                ;;
+            "-l"|"--local")
+                local=1
+                shift 1
+                ;;
+            "-wk"|"--wazuh-key")
+                wazuh_config=1
+                wazuhclusterkey="$2"
+                shift 2
+                ;;
+            "-h"|"--help")
+                getHelp
+                ;;
+            *)
+                getHelp
+        esac
+    done
+
+    importFunction "common.sh"
+    logfile="/var/log/wazuh-unattended-installation.log"
+    importFunction "wazuh-cert-tool.sh"
+    
+    if [ -n "${certificates}" ] || [ -n "${AIO}" ]; then
+        createCertificates
+    fi
+
+    if [ -n "${elastic}" ]; then
+
+        importFunction "elasticsearch.sh"
+
+        if [ -n "${ignore}" ]; then
+            logger -w "Health-check ignored."
+        else
+            healthCheck elastic
+        fi
+        checkSystem
+        installPrerequisites
+        addWazuhrepo
+        checkNodes
+        installElasticsearch 
+        configureElasticsearch
+    fi
+
+    if [ -n "${kibana}" ]; then
+
+        importFunction "kibana.sh"
+
+        if [ -n "${ignore}" ]; then
+            logger -w "Health-check ignored."
+        else
+            healthCheck kibana
+        fi
+        checkSystem
+        installPrerequisites
+        addWazuhrepo
+        installKibana 
+        configureKibana
+    fi
+
+    if [ -n "${wazuh}" ]; then
+
+        if [ -n "$wazuhclusterkey" ] && [ ! -f wazuh_cluster_config.yml ]; then
+            logger -e "No wazuh_cluster_config.yml file found."
+            exit 1;
+        fi
+
+        importFunction "wazuh.sh"
+        importFunction "filebeat.sh"
+
+        if [ -n "${ignore}" ]; then
+            logger -w "Health-check ignored."
+        else
+            healthCheck wazuh
+        fi
+        checkSystem
+        installPrerequisites
+        addWazuhrepo
+        installWazuh
+        if [ -n "$wazuhclusterkey" ]; then
+            configureWazuhCluster 
+        fi  
+        installFilebeat  
+        configureFilebeat
+    fi
+
+    if [ -n "${AIO}" ]; then
+
+        importFunction "wazuh.sh"
+        importFunction "filebeat.sh"
+        importFunction "elasticsearch.sh"
+        importFunction "kibana.sh"
+
+        if [ -n "${ignore}" ]; then
+            logger -w "Health-check ignored."
+        else
+            healthCheck elasticsearch
+            healthCheck kibana
+            healthCheck wazuh
+        fi
+        checkSystem
+        installPrerequisites
+        addWazuhrepo
+        installWazuh
+        installElasticsearch
+        configureElasticsearchAIO
+        installFilebeat
+        configureFilebeatAIO
+        installKibana
+        configureKibanaAIO
+    fi
+}
+
+main "$@"
