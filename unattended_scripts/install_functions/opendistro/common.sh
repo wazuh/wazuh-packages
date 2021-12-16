5ca17d1ef7496dae4bae9e7ab6b64b9012383a18
dfolcha <daniel.folch@wazuh.com>
2021-12-16 17:25:07 +0100

    Add new config.yml and use new function to read yml

--- unattended_scripts/install_functions/opendistro/common.sh
+++ unattended_scripts/install_functions/opendistro/common.sh
@@ -399,3 +399,38 @@ rollBack() {
         logger -w "Installation cleaned. Check the /var/log/wazuh-unattended-installation.log file to learn more about the issue."
     fi
 }
+
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+readConfig() {
+    vars=$(parse_yaml ${base_path}/config.yml)
+    eval $vars
+    vars="elasticsearch_node_names=( $(parse_yaml ${base_path}/config.yml | grep certificates_elasticsearch_name | sed 's/certificates_elasticsearch_name=//') )"
+    eval $vars
+    vars="wazuh_servers_node_names=( $(parse_yaml ${base_path}/config.yml | grep certificates_wazuh_servers_name | sed 's/certificates_wazuh_servers_name=//') )"
+    eval $vars
+    vars="kibana_node_names=( $(parse_yaml ${base_path}/config.yml | grep certificates_kibana_name | sed 's/certificates_kibana_name=//') )"
+    eval $vars
+
+    vars="elasticsearch_node_ips=( $(parse_yaml ${base_path}/config.yml | grep certificates_elasticsearch_ip | sed 's/certificates_elasticsearch_ip=//') )"
+    eval $vars
+    vars="wazuh_servers_node_ips=( $(parse_yaml ${base_path}/config.yml | grep certificates_wazuh_servers_ip | sed 's/certificates_wazuh_servers_ip=//') )"
+    eval $vars
+    vars="kibana_node_ips=( $(parse_yaml ${base_path}/config.yml | grep certificates_kibana_ip | sed 's/certificates_kibana_ip=//') )"
+    eval $vars
+}
