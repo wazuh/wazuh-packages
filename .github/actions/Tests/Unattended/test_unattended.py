87b0f35931b3e4e158dfc5f28fe9663a2b57f91a
Gonzalo Acuña <teddytpc1@gmail.com>
2022-01-04 08:50:13 -0300

    Unattended tests updated

--- .github/actions/Tests/Unattended/test_unattended.py
+++ .github/actions/Tests/Unattended/test_unattended.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pytest
 import time
 import os
 import re
@@ -35,6 +36,11 @@ def get_elasticsearch_username():
     dictionary = yaml.safe_load(stream)
     return (dictionary.get('output.elasticsearch','username').get('username'))
 
+def get_elasticsearch_ip():
+    stream = open("/etc/elasticsearch/elasticsearch.yml", 'r')
+    dictionary = yaml.safe_load(stream)
+    return (dictionary.get('network.host'))
+
 def get_kibana_password():
     stream = open("/etc/kibana/kibana.yml", 'r')
     dictionary = yaml.safe_load(stream)
@@ -46,21 +52,24 @@ def get_kibana_username():
     return (dictionary.get('elasticsearch.username'))
 
 def get_elasticsearch_cluster_status():
-    resp = requests.get('https://localhost:9200/_cluster/health',
+    ip = get_elasticsearch_ip()
+    resp = requests.get('https://'+ip+':9200/_cluster/health',
                         auth=(get_elasticsearch_username(), 
                         get_elasticsearch_password()), 
                         verify=False)
     return (resp.json()['status'])
 
 def get_kibana_status():
-    resp = requests.get('https://localhost',
+    ip = get_elasticsearch_ip()
+    resp = requests.get('https://'+ip,
                         auth=(get_kibana_username(), 
                         get_kibana_password()), 
                         verify=False)
     return (resp.status_code)
 
 def get_kibana_status():
-    resp = requests.get('https://localhost',
+    ip = get_elasticsearch_ip()
+    resp = requests.get('https://'+ip,
                         auth=(get_kibana_username(), 
                         get_kibana_password()), 
                         verify=False)
@@ -69,7 +78,7 @@ def get_kibana_status():
 def get_wazuh_api_status():
 
     protocol = 'https'
-    host = 'localhost'
+    host = get_elasticsearch_ip()
     port = 55000
     user = 'wazuh'
     password = 'wazuh'
@@ -89,56 +98,76 @@ def get_wazuh_api_status():
 
 # ----------------------------- Tests ----------------------------- 
 
-
-
+@pytest.mark.every
 def test_check_wazuh_manager_authd():
     assert check_call("ps -xa | grep wazuh-authd | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_db():
     assert check_call("ps -xa | grep wazuh-db | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_execd():
     assert check_call("ps -xa | grep wazuh-execd | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_analysisd():
     assert check_call("ps -xa | grep wazuh-analysisd | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_syscheckd():
     assert check_call("ps -xa | grep wazuh-syscheckd | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_remoted():
     assert check_call("ps -xa | grep wazuh-remoted | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_logcollec():
     assert check_call("ps -xa | grep wazuh-logcollec | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_monitord():
     assert check_call("ps -xa | grep wazuh-monitord | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_modulesd():
     assert check_call("ps -xa | grep wazuh-modulesd | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_wazuh_manager_apid():
     assert check_call("ps -xa | grep wazuh-apid | grep -v grep", shell=True) != ""
 
+@pytest.mark.distributed
+def test_check_wazuh_manager_clusterd():
+    assert check_call("ps -xa | grep wazuh-clusterd | grep -v grep", shell=True) != ""
+
+@pytest.mark.every
 def test_check_filebeat_process():
     assert check_call("ps -xa | grep \"/usr/share/filebeat/bin/filebeat\" | grep -v grep", shell=True) != ""
 
+@pytest.mark.every
 def test_check_elasticsearch_process():
     assert check_call("ps -xa | grep \"/usr/share/elasticsearch/jdk/bin/java\" | grep -v grep | cut -d \" \" -f15", shell=True) != ""
 
+@pytest.mark.master
 def test_check_kibana_process():
     assert check_call("ps -xa | grep \"/usr/share/kibana/bin/../node/bin/node\" | grep -v grep", shell=True) != ""
 
+@pytest.mark.master
 def test_check_elasticsearch_cluster_status():
     assert get_elasticsearch_cluster_status() != "red"
+    assert get_elasticsearch_cluster_status() != "yellow" 
 
+@pytest.mark.master
 def test_check_kibana_status():
     assert get_kibana_status() == 200
 
+@pytest.mark.every
 def test_test_check_wazuh_api_status():
     assert get_wazuh_api_status() == "Wazuh API REST"
 
+@pytest.mark.every
 def test_check_log_errors():
     found_error = False
     with open('/var/ossec/logs/ossec.log', 'r') as f:
