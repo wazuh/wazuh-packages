5b8fe66062ab64e870fd38e4a8a4e0b8e4cf488e
verdx <verdx@riseup.net>
2022-02-07 11:11:51 +0100

    Fixed modifications to filebeat configuration file
    Signed-off-by: verdx <verdx@riseup.net>

--- unattended_installer/install_functions/filebeat.sh
+++ unattended_installer/install_functions/filebeat.sh
@@ -18,10 +18,10 @@ function filebeat_configure(){
     else
         eval "getConfig filebeat/filebeat_distributed.yml /etc/filebeat/filebeat.yml ${debug}"
         if [ ${#indexer_node_names[@]} -eq 1 ]; then
-            echo "output.elasticsearch.hosts:" >> /etc/filebeat/filebeat.yml
+            echo -e "\noutput.elasticsearch.hosts:" >> /etc/filebeat/filebeat.yml
             echo "  - ${indexer_node_ips[0]}:9700" >> /etc/filebeat/filebeat.yml
         else
-            echo "output.elasticsearch.hosts:" >> /etc/filebeat/filebeat.yml
+            echo -e "\noutput.elasticsearch.hosts:" >> /etc/filebeat/filebeat.yml
             for i in "${indexer_node_ips[@]}"; do
                 echo "  - ${i}:9700" >> /etc/filebeat/filebeat.yml
             done
