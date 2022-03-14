5e4493e49fb6e75c8e67aee649f59e917bb2f11c
rauldpm <raul.delpozo@wazuh.com>
2022-03-14 18:36:00 +0100

    Added spaces and tab regex

--- unattended_installer/passwords_tool/passwordsFunctions.sh
+++ unattended_installer/passwords_tool/passwordsFunctions.sh
@@ -217,7 +217,7 @@ function passwords_readAdmincerts() {
 }
 
 function passwords_readFileUsers() {
-    filecorrect=$(grep -Ev '^#|^\s*$' "${p_file}" |  grep -Pzc '\A(\s*username:\s*\w+\s*password:\s*[A-Za-z0-9_\-]+\s*)+\Z')
+    filecorrect=$(grep -Ev '^#|^\s*$' "${p_file}" | grep -Pzc '\A(\s*username:[ \t]+\w+\s*password:[ \t]+[A-Za-z0-9_\-]+\s*)+\Z')
     if [[ "${filecorrect}" -ne 1 ]]; then
         common_logger -e "The password file doesn't have a correct format.
 
