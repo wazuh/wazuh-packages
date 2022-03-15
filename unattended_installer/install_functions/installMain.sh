0efbe554cdfcc56e9cafe5ccfcaad04e2c26f482
c-bordon <carlos.e.bordon@gmail.com>
2022-03-15 12:23:28 -0300

    Add rm to certs dir

--- unattended_installer/install_functions/installMain.sh
+++ unattended_installer/install_functions/installMain.sh
@@ -241,6 +241,7 @@ function main() {
         eval "chown root:root ${base_path}/wazuh-install-files/*"
         eval "tar -zcf '${tar_file}' -C '${base_path}/' wazuh-install-files/ ${debug}"
         eval "rm -rf '${base_path}/wazuh-install-files' ${debug}"
+        eval "rm -rf '${base_path}/certs' ${debug}"
         common_logger "Created ${tar_file}. Contains Wazuh cluster key, certificates, and passwords necessary for installation."
     fi
 
