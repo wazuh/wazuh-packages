7613d51eaa7bd922e51b0cbb4259a5c5668fda91
Federico Caffieri <89791732+fcaffieri@users.noreply.github.com>
2022-03-28 11:00:41 -0300

    Update script.sh

--- .github/actions/shellcheck/script.sh
+++ .github/actions/shellcheck/script.sh
@@ -54,7 +54,7 @@ echo $FILES
 
 echo '::group:: Running shellcheck ...'
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
-  shellcheck -f json  ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} artifacts_generated/wazuh-passwords-tool.sh \
+  shellcheck -f json  ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} ./unattended_installer/common_functions/common.sh \
     | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
     | reviewdog \
         -efm="%f:%l:%c:%t%*[^:]:%m" \
