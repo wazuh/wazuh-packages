24a784b59cfb8c5a71f8a328c302336e692a0076
dfolcha <daniel.folch@wazuh.com>
2022-04-04 17:20:27 +0200

    Fix copyright

--- aix/generate_wazuh_packages.sh
+++ aix/generate_wazuh_packages.sh
@@ -1,7 +1,7 @@
-#!/bin/ksh
+#!/bin/bash
 
-# Script to build Wazuh RPM package for AIX
-# Copyright (C) 2015-2020, Wazuh Inc.
+# Wazuh package generator
+# Copyright (C) 2015, Wazuh Inc.
 #
 # This program is a free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public
