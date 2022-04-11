b7137833cbfb1d7d9800865949ef3e7931639254
miguelfdez99 <miguelfdez99@gmail.com>
2022-04-11 14:18:43 +0200

    update indexer-security-init.sh

--- stack/indexer/indexer-security-init.sh
+++ stack/indexer/indexer-security-init.sh
@@ -12,11 +12,14 @@ HOST=""
 INSTALL_PATH="/usr/share/wazuh-indexer"
 CONFIG_PATH="/etc/wazuh-indexer"
 OPTIONS="-icl -nhnv"
+WAZUH_INDEXER_ROOT_CA="$(cat /etc/wazuh-indexer/opensearch.yml 2>&1 | grep http.pemtrustedcas | sed 's/.*: //')"
+WAZUH_INDEXER_ADMIN_PATH="$(dirname $WAZUH_INDEXER_ROOT_CA 2>&1)"
+SECURITY_PATH="${INSTALL_PATH}/plugins/opensearch-security"
 
 # -----------------------------------------------------------------------------
 
 getNetworkHost() {
-    HOST=$(grep -hr "network.host:" "${CONFIG_PATH}"/opensearch.yml)
+    HOST=$(grep -hr "network.host:" "${CONFIG_PATH}"/opensearch.yml 2>&1)
     NH="network.host: "
     HOST="${HOST//$NH}"
 
@@ -33,7 +36,7 @@ getNetworkHost() {
 
 # -----------------------------------------------------------------------------
 
-PORT=$(grep -hr 'transport.tcp.port' "${CONFIG_PATH}/opensearch.yml")
+PORT=$(grep -hr 'transport.tcp.port' "${CONFIG_PATH}/opensearch.yml" 2>&1)
 if [ "${PORT}" ]; then
     PORT=$(echo ${PORT} | cut -d' ' -f2 | cut -d'-' -f1)
 else
@@ -42,17 +45,15 @@ fi
 
 # -----------------------------------------------------------------------------
 
-WAZUH_INDEXER_ROOT_CA="$(cat /etc/wazuh-indexer/opensearch.yml | grep http.pemtrustedcas | sed 's/.*: //')"
-WAZUH_INDEXER_ADMIN_PATH="$(dirname $WAZUH_INDEXER_ROOT_CA)"
 securityadmin() {
-SECURITY_PATH="${INSTALL_PATH}/plugins/opensearch-security"
 
-if [ -f "${WAZUH_INDEXER_ADMIN_PATH}/admin.pem" ] && [ -f "${WAZUH_INDEXER_ADMIN_PATH}/admin-key.pem" ] && [ -f "${WAZUH_INDEXER_ROOT_CA}" ]; then
-    OPENSEARCH_PATH_CONF="${CONFIG_PATH}" JAVA_HOME="${INSTALL_PATH}/jdk" runuser wazuh-indexer --shell="/bin/bash" --command="${SECURITY_PATH}/tools/securityadmin.sh -cd ${SECURITY_PATH}/securityconfig -cacert ${WAZUH_INDEXER_ROOT_CA} -cert ${WAZUH_INDEXER_ADMIN_PATH}/admin.pem -key ${WAZUH_INDEXER_ADMIN_PATH}/admin-key.pem -h ${HOST} -p ${PORT} ${OPTIONS}"
-else
-    echo "ERROR: admin.pem and admin-key.pem could not be found in ${WAZUH_INDEXER_ADMIN_PATH}. You must execute the following command:" JAVA_HOME="/usr/share/wazuh-indexer/jdk" runuser wazuh-indexer --shell="/bin/bash" --command="/usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/wazuh-indexer/plugins/opensearch-security/plugins/opensearch-security/securityconfig -cacert ${WAZUH_INDEXER_ROOT_CA} -cert $/etc/wazuh-indexer/certs/admin.pem -key /etc/wazuh-indexer/certs/admin-key.pem -h ${HOST} -p ${PORT} ${OPTIONS}"" "
-    exit 1
-fi
+    if [ -f "${WAZUH_INDEXER_ADMIN_PATH}/admin.pem" ] && [ -f "${WAZUH_INDEXER_ADMIN_PATH}/admin-key.pem" ] && [ -f "${WAZUH_INDEXER_ROOT_CA}" ]; then
+        OPENSEARCH_PATH_CONF="${CONFIG_PATH}" JAVA_HOME="${INSTALL_PATH}/jdk" runuser wazuh-indexer --shell="/bin/bash" --command="${SECURITY_PATH}/tools/securityadmin.sh -cd ${SECURITY_PATH}/securityconfig -cacert ${WAZUH_INDEXER_ROOT_CA} -cert ${WAZUH_INDEXER_ADMIN_PATH}/admin.pem -key ${WAZUH_INDEXER_ADMIN_PATH}/admin-key.pem -h ${HOST} -p ${PORT} ${OPTIONS}"
+    else
+        echo "ERROR: this tool try to find admin.pem and admin-key.pem in ${WAZUH_INDEXER_ADMIN_PATH} but it couldn't. In this case, you must run manually the Indexer security initializer by running the command: JAVA_HOME="/usr/share/wazuh-indexer/jdk" runuser wazuh-indexer --shell="/bin/bash" --command="/usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cd /usr/share/wazuh-indexer/plugins/opensearch-security/plugins/opensearch-security/securityconfig -cacert /path/to/root-ca.pem -cert /path/to/admin.pem -key /path/to/admin-key.pem -h ${HOST} -p ${PORT} ${OPTIONS}" replacing /path/to/ by your certificates path."
+        exit 1
+    fi
+
 }
 
 help() {
