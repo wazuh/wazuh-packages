7160664481d689403e309a143071bfc17270a7fe
c-bordon <carlos.e.bordon@gmail.com>
2022-04-12 10:22:05 -0300

    Changed CONFIG_DIR var in opensearch-dashboards script

--- stack/dashboard/base/builder.sh
+++ stack/dashboard/base/builder.sh
@@ -56,6 +56,9 @@ cp ./etc/custom_welcome/Assets/Favicons/* ./src/core/server/core_app/assets/favi
 cp ./etc/custom_welcome/Assets/Favicons/favicon-32x32.png ./src/core/server/core_app/assets/favicons/favicon.ico
 cp ./etc/opensearch_dashboards_config.js ./src/core/server/opensearch_dashboards_config.js
 cp ./etc/http_service.js ./src/core/server/http/http_service.js
+cp ./etc/opensearch-dashboards ./bin/opensearch-dashboards
+cp ./etc/opensearch-dashboards-keystore ./bin/opensearch-dashboards-keystore
+cp ./etc/opensearch-dashboards-plugin ./bin/opensearch-dashboards-plugin
 # Replace the redirection to `home` in the header logo
 sed -i "s'/app/home'/app/wazuh'g" ./src/core/target/public/core.entry.js
 # Replace others redirections to `home`
