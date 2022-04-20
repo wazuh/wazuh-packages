42b53521c7103920997c1c021c2415faf4939bc1
dfolcha <daniel.folch@wazuh.com>
2022-04-20 16:54:35 +0200

    Change caps

--- unattended_installer/install_functions/wazuh-offline-download.sh
+++ unattended_installer/install_functions/wazuh-offline-download.sh
@@ -23,32 +23,31 @@ function offline_download() {
   case "${package_type}" in
     "deb")
       for p in ${WAZUH_DEB_PACKAGES[@]}; do
-          # Download packages for Wazuh
+          # Download packages for Wazuh manager
           curl -so ${DEST_PATH}/$p ${WAZUH_DEB_BASE_URL}/$p
-          common_logger "Wazuh Manager deb package downloaded"
+          common_logger "Wazuh manager deb package downloaded"
       done
-
       for p in ${FILEBEAT_DEB_PACKAGES[@]}; do
           # Download packages for Filebeat
           curl -so ${DEST_PATH}/$p ${FILEBEAT_DEB_BASE_URL}/$p
           common_logger "Filebeat deb package downloaded"
       done
       for p in ${INDEXER_DEB_PACKAGES[@]}; do
-          # Download packages for Wazuh Indexer
+          # Download packages for Wazuh indexer
           curl -so ${DEST_PATH}/$p ${INDEXER_DEB_BASE_URL}/$p
-          common_logger "Wazuh Indexer deb package downloaded"
+          common_logger "Wazuh indexer deb package downloaded"
       done
       for p in ${DASHBOARD_DEB_PACKAGES[@]}; do
-          # Download packages for Wazuh Dashboard
+          # Download packages for Wazuh dashboard
           curl -so ${DEST_PATH}/$p ${DASHBOARD_DEB_BASE_URL}/$p
-          common_logger "Wazuh Dashboard deb package downloaded"
+          common_logger "Wazuh dashboard deb package downloaded"
       done
     ;;
     "rpm")
       for p in ${WAZUH_RPM_PACKAGES[@]}; do
-          # Download packages for Wazuh
+          # Download packages for Wazuh manager
           curl -so ${DEST_PATH}/$p ${WAZUH_RPM_BASE_URL}/$p
-          common_logger "Wazuh Manager rpm package downloaded"
+          common_logger "Wazuh manager rpm package downloaded"
       done
       for p in ${FILEBEAT_RPM_PACKAGES[@]}; do
           # Download packages for Filebeat
@@ -58,12 +57,12 @@ function offline_download() {
       for p in ${INDEXER_RPM_PACKAGES[@]}; do
           # Download packages for Wazuh Indexer
           curl -so ${DEST_PATH}/$p ${INDEXER_RPM_BASE_URL}/$p
-          common_logger "Wazuh Indexer rpm package downloaded"
+          common_logger "Wazuh indexer rpm package downloaded"
       done
       for p in ${DASHBOARD_RPM_PACKAGES[@]}; do
           # Download packages for Wazuh Dashboard
           curl -so ${DEST_PATH}/$p ${DASHBOARD_RPM_BASE_URL}/$p
-          common_logger "Wazuh Dashboard rpm package downloaded"
+          common_logger "Wazuh dashboard rpm package downloaded"
       done
     ;;
     *)
