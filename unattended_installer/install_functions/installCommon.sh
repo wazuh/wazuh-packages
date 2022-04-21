d8552621594c6b1219c32a46c009a961a8cd129d
miguelfdez99 <miguelfdez99@gmail.com>
2022-04-21 13:24:33 +0200

    Added -q in grep to hide apt lock error message

--- unattended_installer/install_functions/installCommon.sh
+++ unattended_installer/install_functions/installCommon.sh
@@ -78,14 +78,14 @@ function installCommon_aptInstall() {
     fi
     eval "DEBIAN_FRONTEND=noninteractive apt-get install ${installer} -y -q ${debug}"
     install_result="$?"
-    eval "tail -n 2 ${logfile} | grep 'Could not get lock'"
+    eval "tail -n 2 ${logfile} | grep -q 'Could not get lock'"
     grep_result="$?"
     while [ "${grep_result}" -eq 0 ] && [ "${i}" -lt 12 ]; do
         sleep 10
         i=$((i+1))
         eval "DEBIAN_FRONTEND=noninteractive apt-get install ${installer} -y -q ${debug}"
         install_result="$?"
-        eval "tail -n 2 ${logfile} | grep 'Could not get lock'"
+        eval "tail -n 2 ${logfile} | grep -q 'Could not get lock'"
         grep_result="$?"
     done
 
