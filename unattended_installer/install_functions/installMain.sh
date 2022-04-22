7deb96ac49b6b0b7120e44a9049a42f09e5a9552
c-bordon <carlos.e.bordon@gmail.com>
2022-04-22 15:47:11 -0300

    Change validation and message

--- unattended_installer/install_functions/installMain.sh
+++ unattended_installer/install_functions/installMain.sh
@@ -82,9 +82,11 @@ function main() {
                 common_checkRoot
             fi
         fi
-        if [ "${args}" == "-wi" ] && [ "${args}" == "-wd" ] && [ "${args}" == "-ws" ] && [ "${args}" == "--wazuh-dashboard" ] && [ "${args}" == "--wazuh-indexer" ] && [ "${args}" == "--wazuh-server" ]; then
+        if [ "${args}" == "-wi" ] || [ "${args}" == "-wd" ] || [ "${args}" == "-ws" ] || [ "${args}" == "--wazuh-dashboard" ] || [ "${args}" == "--wazuh-indexer" ] || [ "${args}" == "--wazuh-server" ]; then
             if [[ "${2}" == -* ]]; then
-                common_logger -e "Error on argument ${args}. Probably missing <node-name> after ${args} or ${2} Can't start with -"
+                common_logger -e "Error on argument ${args}. Check the given <node-name>, the parameter ${2} cannot start with \"-\" or is incorrect"
+                getHelp
+                exit 1
             fi
         fi
     done
