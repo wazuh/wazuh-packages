f018f0f9b3cec28d8051add56955987b8015d36e
Hanes Sciarrone <58960358+HanesSciarrone@users.noreply.github.com>
2022-04-24 21:20:39 -0300

    Download cv2pdb, generate zip file with debug symbols for winagent (#1452)
    * Build the zip file with debug symbols files for the built package.
    
    * Rename an option in the generate_compiled_windows_agent.sh script
    
    * Remove the unnecessary variable, improve the description of a new option in the script, and change the if condition.
    
    * Expanded explanation of the new option in the script.
    
    * Changed description of new option in the generator script.
    
    * Improve explanation.
    
    * Added newline empty and indentation of if condition.

--- windows/entrypoint.sh
+++ windows/entrypoint.sh
@@ -15,15 +15,31 @@ wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
 
 # Compile the wazuh agent for Windows
 FLAGS="-j ${JOBS} "
+VERSION=$(cat wazuh-*/src/VERSION | cut -dv -f2)
+ZIP_SYMBOLS_NAME="wazuh-agent-windows-x86_64-debug-info-${VERSION}-${REVISION}.zip"
 
 if [[ "${DEBUG}" = "yes" ]]; then
     FLAGS+="-d "
 fi
 
+rm /wazuh-*/src/symbols/.gitignore
+
 make -C /wazuh-*/src deps TARGET=winagent ${FLAGS}
-make -C /wazuh-*/src TARGET=winagent ${FLAGS}
+
+if [ ! -z "/tools" ]; then
+    make -C /wazuh-*/src TARGET=winagent ${FLAGS} WIN_STRIP_TOOL_PATH=/tools
+else
+    make -C /wazuh-*/src TARGET=winagent ${FLAGS}
+fi
+
+if [ "$(ls -A /wazuh-*/src/symbols)" ]; then
+    cd /wazuh-*/src/symbols && zip -r ${ZIP_SYMBOLS_NAME} .
+    cd /wazuh-*/src/symbols && mv ${ZIP_SYMBOLS_NAME} /shared
+    cd /
+fi
 
 rm -rf /wazuh-*/src/external
+rm -rf /wazuh-*/src/symbols
 
 # Zip the compiled agent and move it to the shared folder
 zip -r ${ZIP_NAME} wazuh-*
