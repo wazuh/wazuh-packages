0b9398805cb1f01c8feeb4f16f537310c3335559
dfolcha <daniel.folch@wazuh.com>
2022-04-28 10:27:29 +0200

    Remove message

--- unattended_installer/install_functions/dashboard.sh
+++ unattended_installer/install_functions/dashboard.sh
@@ -127,13 +127,11 @@ function dashboard_initialize() {
         else
             common_logger "--- Summary ---"
             common_logger "When Wazuh dashboard is able to connect to your Wazuh indexer cluster, you can access the web interface https://${nodes_dashboard_ip}.\n    User: admin\n    Password: ${u_pass}"
-            common_logger "The password can also be seen in the file ${logfile}"
         fi
     else
         common_logger "Wazuh dashboard web application initialized."
         common_logger "--- Summary ---"
         common_logger "You can access the web interface https://${nodes_dashboard_ip}.\n    User: admin\n    Password: ${u_pass}"
-        common_logger "The password can also be seen in the file ${logfile}"
     fi
 
 }
@@ -155,7 +153,6 @@ function dashboard_initializeAIO() {
     common_logger "Wazuh dashboard web application initialized."
     common_logger "--- Summary ---"
     common_logger "You can access the web interface https://<wazuh-dashboard-ip>.\n    User: admin\n    Password: ${u_pass}"
-    common_logger "The password can also be seen in the file ${logfile}"
 
 }
 
