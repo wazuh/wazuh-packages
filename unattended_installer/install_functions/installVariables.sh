5c115308874d2b777d4dd2707943eba36def6937
dfolcha <daniel.folch@wazuh.com>
2022-05-09 12:17:21 +0200

    Update installer assistant version

--- unattended_installer/install_functions/installVariables.sh
+++ unattended_installer/install_functions/installVariables.sh
@@ -7,14 +7,14 @@
 # Foundation.
 
 ## Package vars
-readonly wazuh_major="4.3"
-readonly wazuh_version="4.3.0"
+readonly wazuh_major="4.4"
+readonly wazuh_version="4.4.0"
 readonly wazuh_revision_deb="1"
 readonly wazuh_revision_rpm="1"
 readonly indexer_revision_deb="1"
 readonly indexer_revision_rpm="1"
 readonly dashboard_revision_deb="1"
-readonly dashboard_revision_rpm="2"
+readonly dashboard_revision_rpm="1"
 readonly filebeat_version="7.10.2"
 readonly wazuh_install_vesion="0.1"
 
