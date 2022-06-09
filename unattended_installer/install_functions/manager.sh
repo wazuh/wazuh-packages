dfbd4dddf2b956a39d386f4565b2045d4be524f2
miguelfdez99 <miguelfdez99@gmail.com>
2022-06-09 12:52:24 +0200

    Remove version variables

--- unattended_installer/install_functions/manager.sh
+++ unattended_installer/install_functions/manager.sh
@@ -45,13 +45,13 @@ function manager_install() {
 
     common_logger "Starting the Wazuh manager installation."
     if [ "${sys_type}" == "zypper" ]; then
-        eval "${sys_type} -n install wazuh-manager=${wazuh_version}-${wazuh_revision_rpm} ${debug}"
+        eval "${sys_type} -n install wazuh-manager=${wazuh_version} ${debug}"
         install_result="${PIPESTATUS[0]}"
     elif [ "${sys_type}" == "yum" ]; then
-        eval "${sys_type} install wazuh-manager${sep}${wazuh_version}-${wazuh_revision_rpm} -y ${debug}"
+        eval "${sys_type} install wazuh-manager${sep}${wazuh_version} -y ${debug}"
         install_result="${PIPESTATUS[0]}"
     elif [ "${sys_type}" == "apt-get" ]; then
-        installCommon_aptInstall "wazuh-manager" "${wazuh_version}-${wazuh_revision_deb}"
+        installCommon_aptInstall "wazuh-manager" "${wazuh_version}-*"
     fi
     
     common_checkInstalled
