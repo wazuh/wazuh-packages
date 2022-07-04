855a2f372856d6fddd7f94a135670c43da4611a0
dfolcha <daniel.folch@wazuh.com>
2022-07-04 10:00:11 +0200

    Fix assistant not changing api pass on single wazuh node

--- unattended_installer/passwords_tool/passwordsFunctions.sh
+++ unattended_installer/passwords_tool/passwordsFunctions.sh
@@ -109,7 +109,9 @@ function passwords_changeDashboardApiPassword() {
     until [ -n "${file_exists}" ] || [ "${j}" -eq "12" ]; do
         if [ -f "/usr/share/wazuh-dashboard/data/wazuh/config/wazuh.yml" ]; then
             eval 'sed -i "s|password: .*|password: ${1}|g" /usr/share/wazuh-dashboard/data/wazuh/config/wazuh.yml'
-            common_logger "Updated wazuh-wui user password in wazuh dashboard. Remember to restart the service."
+            if [ -z "${AIO}" ] && [ -z "${indexer}" ] && [ -z "${dashboard}" ] && [ -z "${wazuh}" ] && [ -z "${start_indexer_cluster}" ]; then
+                common_logger "Updated wazuh-wui user password in wazuh dashboard. Remember to restart the service."
+            fi
             file_exists=1
         fi
         sleep 5
