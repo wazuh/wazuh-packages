fc9b1261a5e2ea6ca5951d64907a8aa325e75eee
c-bordon <carlos.e.bordon@gmail.com>
2022-07-04 12:14:07 -0300

    Add opensearch validation in dashboard base

--- stack/dashboard/base/builder.sh
+++ stack/dashboard/base/builder.sh
@@ -16,6 +16,8 @@ future="${2}"
 revision="${3}"
 reference="${4}"
 base_dir=/tmp/output/wazuh-dashboard-base
+opensearch_major=$(echo "${opensearch_version}" | cut -d. -f1 )
+opensearch_minor=$(echo "${opensearch_version}" | cut -d. -f2 )
 
 # -----------------------------------------------------------------------------
 
@@ -115,6 +117,11 @@ gzip -c ./plugins/securityDashboards/target/public/securityDashboards.chunk.5.js
 brotli -c ./plugins/securityDashboards/target/public/securityDashboards.chunk.5.js > ./plugins/securityDashboards/target/public/securityDashboards.chunk.5.js.br
 # Add VERSION file
 cp /root/VERSION .
+
+# Custom configs for Opensearch Dashboards 2.0.x
+if [ "${opensearch_major}" = "2" ] && [ "${opensearch_minor}" = "0" ];then
+    echo "entro por el if de la version 2.0"
+fi
 
 # Remove plugins
 /bin/bash ./bin/opensearch-dashboards-plugin remove queryWorkbenchDashboards --allow-root
