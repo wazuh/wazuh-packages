9c08f4e9dd6fba76d09f170b4b84111dea60300e
Miguel Fernandez <miguelfdez99@gmail.com>
2022-07-13 12:07:18 +0200

    Added new script

---
+++ .github/actions/passwords-tool/tests-stack-failure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apiPass="$(cat wazuh-install-files/wazuh-passwords.txt | awk "/username: 'wazuh'/{getline;print;}" | awk '{ print $2 }' | tr -d \' )"
+adminPass="$(cat wazuh-install-files/wazuh-passwords.txt | awk "/username: 'admin'/{getline;print;}" | awk '{ print $2 }' | tr -d \')"
+
+if ! bash wazuh-passwords-tool.sh -u wazuuuh | grep "ERROR"; then
+  exit 1
+elif ! sudo bash wazuh-passwords-tool.sh -u wazuh_user -p password | grep "ERROR"; then
+  exit 1 
+elif ! sudo bash wazuh-passwords-tool.sh -au wazuh -ap "${adminPass}" -u wazuh -p password -A | grep "ERROR"; then
+   exit 1
+elif ! curl -s -u wazuh:wazuh -k -X GET "https://localhost:55000/security/user/authenticate" | grep "Invalid credentials"; then
+   exit 1
+elif ! curl -s -u wazuuh:"${apiPass}" -k -X GET "https://localhost:55000/security/user/authenticate" | grep "Invalid credentials"; then
+   exit 1
+elif ! curl -s -XGET https://localhost:9200/ -u admin:admin -k | grep "Unauthorized"; then
+   exit
+elif ! curl -s -XGET https://localhost:9200/ -u adminnnn:"${adminPass}" -k | grep "Unauthorized"; then
+   exit
+fi
