7917722d618b160f4b7afd3498bd27f0e8da1afa
Miguel Fernandez <miguelfdez99@gmail.com>
2022-07-21 15:23:34 +0200

    Set variables as readonly

--- unattended_installer/install_functions/installVariables.sh
+++ unattended_installer/install_functions/installVariables.sh
@@ -59,7 +59,7 @@ readonly filebeat_config_file="${resources}/tpl/wazuh/filebeat/filebeat.yml"
 adminUser="wazuh"
 adminPassword="wazuh"
 
-wazuh_aio_ports=( 9200 9300 1514 1515 1516 55000 443)
-wazuh_indexer_ports=( 9200 9300 )
-wazuh_manager_ports=( 1514 1515 1516 55000 )
-wazuh_dashboard_ports=( 443 )
+readonly wazuh_aio_ports=( 9200 9300 1514 1515 1516 55000 443)
+readonly wazuh_indexer_ports=( 9200 9300 )
+readonly wazuh_manager_ports=( 1514 1515 1516 55000 )
+readonly wazuh_dashboard_ports=( 443 )
