ec56a9dcd1f3a68deaecb3a77c1a6dbcf9f1c696
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 17:32:30 +0000

    Adding numeric_owner as keyword arguement

--- ova/Ova2Ovf.py
+++ ova/Ova2Ovf.py
@@ -46,7 +46,7 @@ with tarfile.open(srcfile) as t:
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(t)
