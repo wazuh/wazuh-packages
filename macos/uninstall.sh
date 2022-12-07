38efc29348f7fa55c3072dd2427f07ba58f8abfb
verdx <verdx@riseup.net>
2022-12-07 18:08:28 +0100

    Fix uninstall script

--- macos/uninstall.sh
+++ macos/uninstall.sh
@@ -1,29 +1,28 @@
-#/bin/sh
+#!/bin/sh
 
 ## Stop and remove application
-sudo /Library/Ossec/bin/ossec-control stop
-sudo /bin/rm -r /Library/Ossec*
+/Library/Ossec/bin/wazuh-control stop
+/bin/rm -r /Library/Ossec*
 
 ## stop and unload dispatcher
-#sudo /bin/launchctl unload /Library/LaunchDaemons/com.wazuh.agent.plist
+/bin/launchctl unload /Library/LaunchDaemons/com.wazuh.agent.plist
 
 # remove launchdaemons
-sudo /bin/rm -f /Library/LaunchDaemons/com.wazuh.agent.plist
+/bin/rm -f /Library/LaunchDaemons/com.wazuh.agent.plist
 
 ## remove StartupItems
-sudo /bin/rm -rf /Library/StartupItems/WAZUH
+/bin/rm -rf /Library/StartupItems/WAZUH
 
 ## Remove User and Groups
-sudo /usr/bin/dscl . -delete "/Users/wazuh"
-sudo /usr/bin/dscl . -delete "/Groups/wazuh"
+/usr/bin/dscl . -delete "/Users/wazuh"
+/usr/bin/dscl . -delete "/Groups/wazuh"
 
-sudo /usr/sbin/pkgutil --forget com.wazuh.pkg.wazuh-agent
-sudo /usr/sbin/pkgutil --forget com.wazuh.pkg.wazuh-agent-etc
+/usr/sbin/pkgutil --forget com.wazuh.pkg.wazuh-agent
 
 # In case it was installed via Puppet pkgdmg provider
 
 if [ -e /var/db/.puppet_pkgdmg_installed_wazuh-agent ]; then
-    sudo rm -f /var/db/.puppet_pkgdmg_installed_wazuh-agent
+    rm -f /var/db/.puppet_pkgdmg_installed_wazuh-agent
 fi
 
 echo
