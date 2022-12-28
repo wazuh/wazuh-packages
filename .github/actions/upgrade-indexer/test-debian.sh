433c57b9904ce788b240cdaeff8d4e5bd2a44144
davidcr01 <davidcorreafjb2001@gmail.com>
2022-12-28 10:24:11 +0100

    Improved script (parametrized)

--- .github/actions/upgrade-indexer/test-debian.sh
+++ .github/actions/upgrade-indexer/test-debian.sh
@@ -1,84 +1,69 @@
 #!/bin/bash
-FILES_OLD="/usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig/*"
-FILES_NEW="/etc/wazuh-indexer/opensearch-security/*"
+FILES_OLD="/usr/share/wazuh-indexer/plugins/opensearch-security/securityconfig"
+FILES_NEW="/etc/wazuh-indexer/opensearch-security"
 declare -A files_old
 declare -A files_new
 
 not_equal=false
 
-# Prints associative array of the old files
-function print_old() {
-for KEY in "${!files_old[@]}"; do
-	# Print the KEY value
-	echo "Old-Key: $KEY"
-	# Print the VALUE attached to that KEY
-	echo "Old-Value: ${files_old[$KEY]}"
-done
+# Prints associative array of the files passed by params
+function print_files() {
+    local -n files=$1
+
+    for KEY in "${!files[@]}"; do
+        # Print the KEY value
+        echo "Key: $KEY"
+        # Print the VALUE attached to that KEY
+        echo "Value: ${files[$KEY]}"
+    done
 }
 
-# Prints associative array of the new files
-function print_new() {
-for KEY in "${!files_new[@]}"; do
-	# Print the KEY value
-	echo "New-Key: $KEY"
-	# Print the VALUE attached to that KEY
-	echo "New-Value: ${files_new[$KEY]}"
-done
-}
+# Reads the files passed by param and store their checksum in the array
+function read_files() {
+    local -n files=$2
 
-# Reads the old files and store their checksum in the array
-function read_old_files() {
-	for f in $FILES_OLD; do
-		echo "Processing $f file..."
-		echo "# This is a test" >> $f
-		checksum=`md5sum $f | cut -d " " -f1`
+    for f in $1/*; do
+        if [ -f $f ]; then
+            echo "Processing $f file..."
+            echo "# This is a test" >> $f
+            checksum=`md5sum $f | cut -d " " -f1`
 
-		basename=`basename $f`
-		files_old[$basename]=$checksum
-	done
-}
-
-# Reads the new files and store their checksum in the array
-function read_new_files() {
-	for f in $FILES_NEW; do
-		echo "Processing $f file..."
-		checksum=`md5sum $f | cut -d " " -f1`
-
-		basename=`basename $f`
-		files_new[$basename]=$checksum
-	done
+            basename=`basename $f`
+            files[$basename]=$checksum
+        fi
+    done
 }
 
 # Compare the arrays, the loop ends if a different checksum is detected
-function compare_files() {
-	for i in "${!files_old[@]}"; do
-		echo "Comparing $i file checksum..."
-		if [[ "$files_old[$i]" == "$files_new[$i]" && $not_equal != true ]]; then
-			echo "$i - Same checksum"
-		else
-			echo "$i - Different checksum"
-			not_equal=true
-		fi
-	done
+function compare_arrays() {
+    local -n array_old=$1
+    local -n array_new=$2
+
+    for i in "${!array_old[@]}"; do
+        echo "Comparing $i file checksum..."
+        if [[ "$array_old[$i]" == "$array_new[$i]" && $not_equal != true ]]; then
+            echo "$i - Same checksum"
+        else
+            echo "$i - Different checksum"
+            not_equal=true
+        fi
+    done
 }
 
-echo 'Installing old wazuh-indexer'
-apt-get install ./wazuh-indexer_4.3.7-0.40320.20220825_amd64.deb
+apt-get install ./wazuh-indexer_4.3.10-1_amd64.deb
+read_files "$FILES_OLD" files_old
+echo "Old files..."
+print_files files_old
 
-read_old_files
-
-echo 'Installing new wazuh-indexer'
 apt-get install ./wazuh-indexer_4.5.0-1_amd64.deb
+read_files "$FILES_NEW" files_new
+echo "New files..."
+print_files files_new
 
-read_new_files
-
-print_old
-print_new
-
-compare_files
+compare_arrays files_old files_new
 
 if [ $not_equal == true ]; then
-		echo "Error: different checksums detected"
-		exit 1
+        echo "Error: different checksums detected"
+        exit 1
 fi
 exit 0
