7ab38f697fcc9743eba79cf6b456cc462866abae
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-10 14:43:40 +0100

    Change the relative path used in checkFilebeat for a defined path variable
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- unattended_installer/builder.sh
+++ unattended_installer/builder.sh
@@ -274,7 +274,7 @@ function builder_main() {
 function checkFilebeatURL() {
 
     # Import variables
-    eval "$(grep -E "filebeat_wazuh_template=" "unattended_installer/install_functions/installVariables.sh")"
+    eval "$(grep -E "filebeat_wazuh_template=" "${resources_installer}/installVariables.sh")"
     new_filebeat_url="https://raw.githubusercontent.com/wazuh/wazuh/master/extensions/elasticsearch/7.x/wazuh-template.json"
 
     # Get the response of the URL and check it
