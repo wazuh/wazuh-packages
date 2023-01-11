1ffae1334fff5b6d2cc21466593ae23fc5251a64
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-11 10:18:16 +0100

    Fix conditions for changes
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- unattended_installer/passwords_tool/passwordsFunctions.sh
+++ unattended_installer/passwords_tool/passwordsFunctions.sh
@@ -8,7 +8,7 @@
 
 function passwords_changePassword() {
 
-    if [ -n "${indexer}" || -z "${api}" ]; then
+    if [ -n "${indexer}" ] || [ -z "${api}" ]; then
         eval "mkdir /etc/wazuh-indexer/backup/ 2>/dev/null"
         eval "cp /etc/wazuh-indexer/opensearch-security/* /etc/wazuh-indexer/backup/ 2>/dev/null"
     fi
