463cccfb8c3e7594ba0197357736744cdd996209
c-bordon <carlos.e.bordon@gmail.com>
2023-01-12 17:55:56 -0300

    Development of build.sh for puppet module

--- puppet_module/build.sh
+++ puppet_module/build.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
+set -x
 
 wazuh_branch=$1
-forge_token="08788f6be64beb40e4159f488470d585b6b8ec1edd8374f27a97f0b5b7ab14dc"
+forge_token=$2
 
 wazuh_version=""
 
-build_dir="/pkg"
 destination_dir="/wazuh_puppet_module"
-version_file="${build_dir}/VERSION"
+version_file="VERSION"
 
 download_sources() {
     if ! curl -L https://github.com/wazuh/wazuh-puppet/tarball/${wazuh_branch} | tar zx ; then
         echo "Error downloading the source code from GitHub."
         exit 1
     fi
-    mv wazuh-* ${build_dir}
+    cd wazuh-*
     wazuh_version=$(grep -oP '^WAZUH-PUPPET_VERSION="\K[^"]+' ${version_file} | cut -d 'v' -f 2)
 }
 
 publish_module() {
-    curl -X POST -H "Authorization: Bearer ${forge_token}" -d ${destination_dir}/wazuh-wazuh-${wazuh_version}.tar.gz -H "Content-Type: application/json" https://forgeapi.puppet.com/v3/releases
+    curl -X POST -H "Authorization: Bearer ${forge_token}" -H 'Content-Type: application/json' -d '{"file": "'$(base64 -w 0 ${destination_dir}/cbordon-test-${wazuh_version}.tar.gz)'"}' https://forgeapi.puppet.com/v3/releases
 }
 
 build_module() {
 
     download_sources
 
-    cd ${build_dir}/wazuh-*
-
     pdk build --force --target-dir=${destination_dir}
+
+    ls -la ${destination_dir}
+
+    publish_module
 
     exit 0
 }
