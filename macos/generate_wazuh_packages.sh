5b7438834211fbb0ba4f2721f4cb8f0831ea8122
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-19 10:40:55 +0100

    Full debug for all the generation process
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- macos/generate_wazuh_packages.sh
+++ macos/generate_wazuh_packages.sh
@@ -111,9 +111,6 @@ function sign_binaries() {
             codesign -f --sign "${CERT_APPLICATION_ID}" --entitlements "${ENTITLEMENTS_PATH}" --timestamp --options=runtime --verbose "${bin}"
         done
 
-        ls ${LOGIN_ITEM_PATH}
-        ls ${LAUNCH_DAEMON_PATH}
-
         codesign -f --sign "${CERT_APPLICATION_ID}" --identifier "com.wazuh.example" --entitlements "${ENTITLEMENTS_PATH}" --timestamp --options=runtime --verbose "${LOGIN_ITEM_PATH}/Wazuh" && echo "Correctly signed Login Item Wazuh" || echo "Error signing Login Item Wazuh"
         codesign -f --sign "${CERT_APPLICATION_ID}" --identifier "com.wazuh.example" --entitlements "${ENTITLEMENTS_PATH}" --timestamp --options=runtime --verbose "${LAUNCH_DAEMON_PATH}/com.wazuh.agent.plist" && echo "Correctly signed Launch Daemon" || echo "Error signing Launch Daemon"
         tar -cf "${LOGIN_ITEM_PATH}/Wazuh.tar" -C "${LOGIN_ITEM_PATH}" Wazuh && echo "Correctly tarred Login Item" || echo "Error tarring Login Item"
@@ -121,7 +118,7 @@ function sign_binaries() {
 
         security -v lock-keychain "${KEYCHAIN}" > /dev/null
     fi
-    set +ex
+    #set +ex
 }
 
 function sign_pkg() {
