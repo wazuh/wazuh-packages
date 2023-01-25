e77e3f733221c9bccf2f04181c60e624a225e86d
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-25 17:34:39 +0100

    Change OS on which the images are generated
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/common-tools/setup_docker_ubuntu.sh
+++ .github/actions/common-tools/setup_docker_ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install ca-certificates curl gnupg lsb-release
+apt-get install -y -q ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y -q docker-ce docker-ce-cli containerd.io
