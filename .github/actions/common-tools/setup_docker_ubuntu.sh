f0ad16dc653a4ddbc0b84b45172e4407fdca527f
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-26 14:13:15 +0100

    Add environment variables to run-on-arch and setup docker on install part
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/common-tools/setup_docker_ubuntu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y -q ca-certificates curl gnupg lsb-release
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y -q docker-ce docker-ce-cli containerd.io
