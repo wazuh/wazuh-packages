42c6fef8248a5d6fdd612fa81f137178faf47959
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-01-30 14:49:18 +0100

    Fix architecture errors for the install and enable tests
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/test-install-enable/install_and_enable.sh
+++ .github/actions/test-install-enable/install_and_enable.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 echo "Installing Wazuh $2."
+
 source /etc/os-release
 if [ "$ID" = "centos" ] && [ "$VERSION_ID" = "8" ]; then
     find /etc/yum.repos.d/ -type f -exec sed -i 's/mirrorlist/#mirrorlist/g' {} \;
     find /etc/yum.repos.d/ -type f -exec sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' {} \;
 fi
-yum install -y "/packages/$1"
+
+if [ -n "$(command -v yum)" ]; then
+    sys_type="yum"
+    sep="-"
+elif [ -n "$(command -v apt-get)" ]; then
+    sys_type="apt-get"
+    sep="="
+else
+    common_logger -e "Couldn'd find type of system"
+    exit 1
+fi
+
+$sys_type install -y "/packages/$1"
 
 echo "Enabling Wazuh $2."
 systemctl enable wazuh-$2
