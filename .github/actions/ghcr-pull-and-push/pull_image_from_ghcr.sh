791cd81f5033546fd74564f3669d4115efcfa246
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-02-01 11:21:29 +0100

    Syntax fixes for the ghcr pushing and pulling scripts
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
@@ -6,18 +6,14 @@ if [ -n "$4" ]; then
 else
     DOCKER_IMAGE_TAG="latest"
 fi
-
+GITHUB_REPOSITORY="wazuh/wazuh-packages"
+GITHUB_OWNER="wazuh"
+IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
 # Login to GHCR
 echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u $GITHUB_USER --password-stdin
 
-# GITHUB_REPOSITORY is always org/repo syntax. Get the owner in case it is different than the actor (when working in an org)
-GITHUB_REPOSITORY="wazuh/wazuh-packages"
-GITHUB_OWNER="wazuh"
-
-# Set up full image with tag
-IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
-IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
-
+# Pull and rename image
 docker pull ${IMAGE_ID}
 docker image tag ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
