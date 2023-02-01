837cae75f21b3c371dd000342474df56c770d8aa
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-02-01 11:21:29 +0100

    Syntax fixes for the ghcr pushing and pulling scripts
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/build_and_push_image_to_ghcr.sh
@@ -8,19 +8,13 @@ if [ -n "$5" ]; then
 else 
     DOCKER_IMAGE_TAG="latest"
 fi
-
-
-# Login to GHCR
-echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u $GITHUB_USER --password-stdin
-
-# GITHUB_REPOSITORY is always org/repo syntax. Get the owner in case it is different than the actor (when working in an org)
 GITHUB_REPOSITORY="wazuh/wazuh-packages"
 GITHUB_OWNER="wazuh"
-
-# Set up full image with tag
 IMAGE_ID=ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
 IMAGE_ID=$(echo ${IMAGE_ID} | tr '[A-Z]' '[a-z]')
 
+# Login to GHCR
+echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u $GITHUB_USER --password-stdin
 
 # Build image
 echo build -t ${IMAGE_ID} -f ${DOCKERFILE_PATH} ${BUILD_CONTEXT}
