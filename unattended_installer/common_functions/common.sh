22281b77dfeb7ca235f43875ecb890cf72ae990a
davidcr01 <davidcorreafjb2001@gmail.com>
2023-02-07 08:59:12 +0100

    Added --noproxy parameter to common_curl

--- unattended_installer/common_functions/common.sh
+++ unattended_installer/common_functions/common.sh
@@ -138,16 +138,16 @@ function common_checkWazuhConfigYaml() {
 function common_curl() {
 
     if [ -n "${curl_has_connrefused}" ]; then
-        eval "curl $@ --retry-connrefused"
+        eval "curl $@ --retry-connrefused --noproxy '*'"
         e_code="${PIPESTATUS[0]}"
     else
         retries=0
-        eval "curl $@"
+        eval "curl $@ --noproxy '*'"
         e_code="${PIPESTATUS[0]}"
         while [ "${e_code}" -eq 7 ] && [ "${retries}" -ne 12 ]; do
             retries=$((retries+1))
             sleep 5
-            eval "curl $@"
+            eval "curl $@ --noproxy '*'"
             e_code="${PIPESTATUS[0]}"
         done
     fi
