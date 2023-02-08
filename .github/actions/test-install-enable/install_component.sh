ad3ba79ec4e3371c22ebb0ce032a118f5779b016
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-02-08 17:07:14 +0100

    Remove zypper machines from the tests
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- .github/actions/test-install-enable/install_component.sh
+++ .github/actions/test-install-enable/install_component.sh
@@ -13,8 +13,6 @@ elif [ -n "$(command -v apt-get)" ]; then
     sys_type="apt-get"
     apt-get update
     apt-get install -y systemd
-elif [ -n "$(command -v zypper)" ]; then
-    sys_type="zypper --no-gpg-checks"
 else
     common_logger -e "Couldn't find type of system"
     exit 1
