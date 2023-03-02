8eeca7a198200963a88688c3e1811c85de683cd7
davidcr01 <davidcorreafjb2001@gmail.com>
2023-03-02 10:06:45 +0100

    Added PDF reports dependencies

--- unattended_installer/install_functions/installMain.sh
+++ unattended_installer/install_functions/installMain.sh
@@ -290,6 +290,7 @@ function main() {
 
     if [ -n "${dashboard}" ]; then
         common_logger "--- Wazuh dashboard ----"
+        dashboard_installDependencies
         dashboard_install
         dashboard_configure
         installCommon_startService "wazuh-dashboard"
@@ -329,6 +330,7 @@ function main() {
         filebeat_configure
         installCommon_startService "filebeat"
         common_logger "--- Wazuh dashboard ---"
+        dashboard_installDependencies
         dashboard_install
         dashboard_configure
         installCommon_startService "wazuh-dashboard"
