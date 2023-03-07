daec2134d626b7baa8a7b24190470b69bd9a1c9c
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-03-07 13:39:47 +0100

    Log when a retry is done
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- unattended_installer/passwords_tool/passwordsFunctions.sh
+++ unattended_installer/passwords_tool/passwordsFunctions.sh
@@ -310,6 +310,7 @@ function passwords_getApiToken() {
     while [[ "${TOKEN_API}" =~ "Wazuh Internal Error" ]] && [ "${retries}" -lt "${max_internal_error_retries}" ]
     do
         TOKEN_API="$(common_curl -s -u \"${adminUser}\":\"${adminPassword}\" -k -X POST \"https://localhost:55000/security/user/authenticate?raw=true\" --max-time 300 --retry 5 --retry-delay 5)"
+        common_logger -e "There was an error while trying to get the API token. Retrying..."
         retries=$((retries+1))
         sleep 1
     done
