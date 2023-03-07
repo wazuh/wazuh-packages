4f3b3b92ae0cc01cabe40327deae312e10fce2c3
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-03-07 13:07:26 +0100

    Remove debug statements and add double-quotes to the TOKEN
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- unattended_installer/passwords_tool/passwordsFunctions.sh
+++ unattended_installer/passwords_tool/passwordsFunctions.sh
@@ -301,13 +301,14 @@ function passwords_generatePasswordFile() {
 function passwords_getApiToken() {
 
     retries=0
-    max_internal_error_retries=5
+    max_internal_error_retries=10
 
-    TOKEN_API=$(common_curl -s -u "${adminUser}":"${adminPassword}" -k -X POST "https://localhost:55000/security/user/authenticate?raw=true" --max-time 300 --retry 5 --retry-delay 5)
+    TOKEN_API="$(common_curl -s -u \"${adminUser}\":\"${adminPassword}\" -k -X POST \"https://localhost:55000/security/user/authenticate?raw=true\" --max-time 300 --retry 5 --retry-delay 5)"
     while [[ "${TOKEN_API}" =~ "Wazuh Internal Error" ]] && [ "${retries}" -lt "${max_internal_error_retries}" ]
     do
-        TOKEN_API=$(common_curl -s -u "${adminUser}":"${adminPassword}" -k -X POST "https://localhost:55000/security/user/authenticate?raw=true" --max-time 300 --retry 5 --retry-delay 5)
+        TOKEN_API="$(common_curl -s -u \"${adminUser}\":\"${adminPassword}\" -k -X POST \"https://localhost:55000/security/user/authenticate?raw=true\" --max-time 300 --retry 5 --retry-delay 5)"
         retries=$((retries+1))
+        sleep 1
     done
     if [[ ${TOKEN_API} =~ "Wazuh Internal Error" ]]; then
         common_logger -e "There was an error while trying to get the API token."
