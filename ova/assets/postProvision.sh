abcc88eff551d37bcdf4b7b916e8878a517c6fcb
c-bordon <carlos.e.bordon@gmail.com>
2023-07-07 14:12:34 -0300

    Restore postProvision.sh

---
+++ ova/assets/postProvision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DEBUG=$1
+[[ ${DEBUG} = "yes" ]] && set -ex || set -e
+
+CURRENT_PATH="$( cd $(dirname $0) ; pwd -P )"
+ASSETS_PATH="${CURRENT_PATH}/assets"
+CUSTOM_PATH="${ASSETS_PATH}/custom"
+SYSTEM_USER="wazuh-user"
+
+systemctl stop wazuh-manager wazuh-indexer filebeat wazuh-dashboard
+
+# Remove everything related to vagrant
+mv ${CUSTOM_PATH}/removeVagrant.service /etc/systemd/system/
+sed -i "s/USER/${SYSTEM_USER}/g" /etc/systemd/system/removeVagrant.service
+mv ${CUSTOM_PATH}/removeVagrant.sh /home/${SYSTEM_USER}/
+sed -i "s/USER/${SYSTEM_USER}/g" /home/${SYSTEM_USER}/removeVagrant.sh
+chmod 755 /home/${SYSTEM_USER}/removeVagrant.sh
+systemctl daemon-reload
+systemctl enable removeVagrant.service
+
+# Clear synced files
+rm -rf ${CURRENT_PATH}/* ${CURRENT_PATH}/.gitignore
+
+# Remove logs
+find /var/log/ -type f -exec bash -c 'cat /dev/null > {}' \;
+find /var/ossec/logs/ -type f -exec bash -c 'cat /dev/null > {}' \;
+
+history -c
