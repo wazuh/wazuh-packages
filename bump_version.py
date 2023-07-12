9e1652f81f7808aa2debfba04f8c9d361bcb5bed
DFolchA <daniel.folch@wazuh.com>
2023-07-12 13:35:19 +0200

    Add functionality to bump rpm spec files

---
+++ bump_version.py
@@ -0,0 +1,48 @@
+import argparse
+import datetime
+import glob, os
+import re
+from packaging.version import Version
+
+format_string = "%m-%d-%Y"
+spec_format_string = "%a %b %d %Y"
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-v', '--version', action='store', dest='version', help='Version to bump to', required=True)
+arg_parser.add_argument('-r', '--revision', action='store', dest='revision', help='Revision to bump to. Default: 1', default=1)
+arg_parser.add_argument('-d', '--date', action='store', dest='date', help='Date to bump to. Format: %m-%d-%Y. Default: today', default=datetime.date.today().strftime('%m-%d-%Y'))
+args = arg_parser.parse_args()
+
+date=datetime.datetime.strptime(args.date, format_string)
+version=Version(args.version)
+
+## Find files to bump .spec, changelog, pkginfo, .pkgproj, test-*.sh, installVariables.sh, CHANGELOG.md, README.md
+spec_files = glob.glob('**/*.spec', recursive=True)
+changelog_files = glob.glob('**/changelog', recursive=True)
+pkginfo_files = glob.glob('**/pkginfo', recursive=True)
+pkgproj_files = glob.glob('**/*.pkgproj', recursive=True)
+test_files = glob.glob('**/test-*.sh', recursive=True)
+install_variables_files = glob.glob('**/installVariables.sh', recursive=True)
+changelog_md_files = glob.glob('**/CHANGELOG.md', recursive=True)
+readme_md_files = glob.glob('**/README.md', recursive=True)
+
+## Bump version in .spec files
+spec_date=date.strftime(spec_format_string)
+for spec_file in spec_files:
+    with open(spec_file, 'r') as file:
+        print('Bumping version in ' + spec_file)
+        filedata = file.read()
+        # Replace version and revision
+        regex = r'Version:\s*(\d+\.\d+\.\d+)'
+        filedata = re.sub(regex, 'Version:     {}'.format(version), filedata)
+        regex = r'Revision:\s*(\d+)'
+        filedata = re.sub(regex, 'Revision:     ' + str(args.revision), filedata)
+        # Add new version to changelog
+        regex = r'%changelog'
+        changelog_string="* {} <info@wazuh.com> - {}\n- More info: https://documentation.wazuh.com/current/release-notes/release-{}-{}-{}.html".format(spec_date, version, version.major, version.minor, version.micro)
+        filedata = re.sub(regex, '%changelog\n' + changelog_string, filedata)
+
+
+    with open(spec_file, 'w') as file:
+        print('Writing version to ' + spec_file)
+        file.write(filedata)
