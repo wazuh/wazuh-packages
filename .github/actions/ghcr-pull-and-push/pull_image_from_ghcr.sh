6d8ef02e291a19bd07fc12dfd2958ae90fb043a2
DFolchA <daniel.folch@wazuh.com>
2023-07-13 10:08:43 +0200

    Add image tag in pull_image_from_ghcr.sh

--- .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
+++ .github/actions/ghcr-pull-and-push/pull_image_from_ghcr.sh
@@ -17,4 +17,4 @@ echo ${GITHUB_PUSH_SECRET} | docker login https://ghcr.io -u $GITHUB_USER --pass
 
 # Pull and rename image
 docker pull ${IMAGE_ID}
-docker image tag ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}
+docker image tag ghcr.io/${GITHUB_OWNER}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
