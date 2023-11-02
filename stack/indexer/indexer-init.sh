778ed8eaa20ceebde21b556d4908405cf594e99f
c-bordon <carlos.e.bordon@gmail.com>
2023-11-02 17:14:30 -0300

    Added new script for indexer ism

---
+++ stack/indexer/indexer-init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Wazuh Copyright (C) 2023 Wazuh Inc. (License GPLv2)
+# Wazuh - indexer initialization script
+
+INSTALL_PATH="/usr/share/wazuh-indexer"
+BIN_PATH="${INSTALL_PATH}/bin"
+
+main() {
+
+    /bin/bash "${BIN_PATH}/indexer-ism-init.sh"
+    /bin/bash "${BIN_PATH}/indexer-security-init.sh"
+
+}
+
+main "$@"
+
