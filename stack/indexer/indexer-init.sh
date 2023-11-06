edc958fcf58aef8203abbdbb66e3cdc903343fe8
c-bordon <carlos.e.bordon@gmail.com>
2023-11-06 12:21:26 -0300

    Changing order of scripts execution

--- stack/indexer/indexer-init.sh
+++ stack/indexer/indexer-init.sh
@@ -7,7 +7,9 @@ BIN_PATH="${INSTALL_PATH}/bin"
 
 main() {
 
+    echo "Executing Wazuh indexer security init script..."
     /bin/bash "${BIN_PATH}/indexer-security-init.sh"
+    echo "Executing Wazuh indexer ism init script..."
     /bin/bash "${BIN_PATH}/indexer-ism-init.sh"
 
 }
