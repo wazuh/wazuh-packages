a4d52cedcdffb2b64462a59ae863a6f44fee7d51
c-bordon <carlos.e.bordon@gmail.com>
2023-11-06 10:53:22 -0300

    Changing order of scripts execution

--- stack/indexer/indexer-init.sh
+++ stack/indexer/indexer-init.sh
@@ -7,8 +7,8 @@ BIN_PATH="${INSTALL_PATH}/bin"
 
 main() {
 
-    /bin/bash "${BIN_PATH}/indexer-ism-init.sh"
     /bin/bash "${BIN_PATH}/indexer-security-init.sh"
+    /bin/bash "${BIN_PATH}/indexer-ism-init.sh"
 
 }
 
