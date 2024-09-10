117f5cc8d41a540b404696e6eed3adf73eefe8f3
Carlos Bordon <64099752+c-bordon@users.noreply.github.com>
2024-09-10 14:13:23 -0300

    Revert "Updated the Wazuh dashboard package revision for offline installation"

--- unattended_installer/install_functions/wazuh-offline-download.sh
+++ unattended_installer/install_functions/wazuh-offline-download.sh
@@ -25,7 +25,7 @@ function offline_download() {
 
   manager_revision="1"
   indexer_revision="1"
-  dashboard_revision="2"
+  dashboard_revision="1"
 
   if [ "${package_type}" == "rpm" ]; then
     manager_rpm_package="wazuh-manager-${wazuh_version}-${manager_revision}.x86_64.${package_type}"
