08a83b1d6e39431b17b98d34e49155133f2cf9aa
Marcel Kemp <marcel.kemp@wazuh.com>
2024-10-04 11:54:38 +0200

    chore: update 4.9.1 release date + source branch

--- unattended_installer/install_functions/installVariables.sh
+++ unattended_installer/install_functions/installVariables.sh
@@ -11,7 +11,7 @@ readonly wazuh_major="4.9"
 readonly wazuh_version="4.9.1"
 readonly filebeat_version="7.10.2"
 readonly wazuh_install_vesion="0.1"
-readonly source_branch="v${wazuh_version}"
+readonly source_branch="v${wazuh_version}-rc3"
 
 ## Links and paths to resources
 readonly resources="https://${bucket}/${wazuh_major}"
